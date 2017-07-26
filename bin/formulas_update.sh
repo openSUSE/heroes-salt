677887f7c0a2018969fb08e46aaa9565f9ae63c1
Theo Chatzimichos <tampakrap@opensuse.org>
2017-07-26 13:51:08 +0200

    Add my nasty script to update formulas
    We need to unhardcode it though

---
+++ bin/formulas_update.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# WARNING: This is a nasty script that needs to be unhardcoded before used
+
+FORMULAS=(
+    dhcpd
+    grains
+    keepalived
+    limits
+    locale
+    mysql
+    ntp
+    openssh
+    powerdns
+    salt
+    sqlite
+    sssd
+    sudoers
+    timezone
+    users
+    zypper
+)
+
+for formula in ${FORMULAS[@]}; do
+    echo "### $formula"
+    case $formula in
+        grains|sqlite|zypper) owner=tampakrap ;;
+        limits) owner=ryancurrah ;;
+        sssd)
+            owner=Spark-Networks
+            $prefix=salt-
+            ;;
+        *) owner=saltstack-formulas ;;
+    esac
+    upstream=git://github.com/$owner/$prefix$formula-formula
+    [[ -d $formula-formula ]] || git clone $upstream
+    pushd $formula-formula > /dev/null
+    git remote set-url origin ${upstream}.git
+    git remote remove opensuse
+    git remote remove suse
+    git remote remove tampakrap
+    git remote add opensuse gitlab@gitlab.infra.opensuse.org:saltstack-formulas/$formula-formula.git
+    git remote add suse gitlab@gitlab.suse.de:salt-formulas/$formula-formula.git
+    git remote add tampakrap git@github.com:tampakrap/$formula-formula.git
+    git remote -v
+    git checkout master
+    git fetch origin
+    git reset --hard origin/master
+    git push -f opensuse master
+    git push -f opensuse master:production
+    git push -f suse master
+    git push -f suse master:production
+    popd > /dev/null
+done
