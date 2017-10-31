c336cc08fea2e36fb82bb79a54fa4038ebe8c302
Theo Chatzimichos <tampakrap@opensuse.org>
2017-10-31 15:23:24 +0100

    add wrapper script to encrypt a string that we can use as secret pillar
    also add a file "encrypted_pillar_recipients" that lists all gpg
    recipients that the script will use

---
+++ bin/encrypt_pillar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+help() {
+    echo "Encrypt a given string and print out the output. This output can be"
+    echo "then used as encrypted pillar"
+    echo
+    echo "Arguments:"
+    echo "-s STRING   The string to encrypt"
+    echo
+}
+
+[[ $1 == '--help' ]] && help && exit
+
+while getopts s:v:h arg; do
+    case ${arg} in
+        s) STRING=${OPTARG} ;;
+        h) help && exit ;;
+        *) help && exit 1 ;;
+    esac
+done
+
+[[ -z $STRING ]] && help && exit 1
+
+RECIPIENTS=$(egrep -v '^(#|$)' encrypted_pillar_recipients | while read i; do echo "-r $i"; done | xargs)
+echo -n "${STRING}" | gpg --armor --batch --trust-model always --encrypt ${RECIPIENTS}
