9a075498d75cd5f594631341a64e3c12acc00036
Theo Chatzimichos <tampakrap@opensuse.org>
2017-10-31 16:17:48 +0100

    bin/encrypt_pillar.sh: read the string from the input
    instead of passing it from an argument, so that we don't have passwords
    on our shell history

--- bin/encrypt_pillar.sh
+++ bin/encrypt_pillar.sh
@@ -4,20 +4,19 @@ help() {
     echo "Encrypt a given string and print out the output. This output can be"
     echo "then used as encrypted pillar"
     echo
-    echo "Arguments:"
-    echo "-s STRING   The string to encrypt"
-    echo
 }
 
 [[ $1 == '--help' ]] && help && exit
 
 while getopts s:v:h arg; do
     case ${arg} in
-        s) STRING=${OPTARG} ;;
         h) help && exit ;;
         *) help && exit 1 ;;
     esac
 done
+
+echo "Please type the string that you want to encrypt"
+read STRING
 
 [[ -z $STRING ]] && help && exit 1
 
