02fadfee17ab22def7df8adb839d8e40e46594aa
Theo Chatzimichos <tampakrap@opensuse.org>
2017-11-11 16:04:18 +0100

    Add test to validate the custom grains in pillar/id/*.sls
    The test validates that the data that are in pillar/id/*.sls match the
    acceptable values as defined in pillar/valid_custom_grains.yaml. The
    data are separated in two groups, global and localized. The global ones
    are checked only if they are defined and if they have valid values. The
    localized ones are also checked based on the defined "country" grain of
    the minion that have realistic values (eg the city matches the country)

---
+++ bin/test_custom_grains.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+# Validates if the pillar/id/$FQDN.sls has correct custom grains
+
+import yaml
+import os
+import sys
+
+
+def error_msg(sls, key, valid_values):
+    if type(valid_values) == str:
+        msg = ' is'
+        result = valid_values
+    else:
+        msg = 's are'
+        result = ', '.join(valid_values)
+    print('pillar/id/%s has invalid value for the "%s" key. Valid value%s: %s' % (sls, key, msg, result))
+    return 1
+
+
+def test_custom_grain(mygrains, sls, key, valid_values, status):
+    try:
+        value = mygrains[key]
+    except KeyError:
+        print('pillar/id/%s is missing the "%s" key' % (sls, key))
+        return 1
+
+    if type(valid_values) == str:
+        if value != valid_values:
+            status = error_msg(sls, key, valid_values)
+    else:
+        if value not in valid_values:
+            status = error_msg(sls, key, valid_values)
+
+    return status
+
+
+status = 0
+
+with open('pillar/valid_custom_grains.yaml', 'r') as f:
+    VALID_CUSTOM_GRAINS = yaml.load(f)
+
+valid_global_grains = VALID_CUSTOM_GRAINS['global']
+all_localized_grains = VALID_CUSTOM_GRAINS['localized']
+
+all_ids = sorted(os.listdir('pillar/id'))
+for sls in all_ids:
+    with open("pillar/id/%s" % sls) as f:
+        mygrains = yaml.load(f)['grains']
+
+        for key, valid_values in valid_global_grains.items():
+            status = test_custom_grain(mygrains, sls, key, valid_values, status)
+
+        try:
+            valid_localized_grains = all_localized_grains[mygrains['country']]
+            for key, valid_values in valid_localized_grains.items():
+                status = test_custom_grain(mygrains, sls, key, valid_values, status)
+        except KeyError:
+            status = error_msg(sls, 'country', all_localized_grains.keys())
+
+sys.exit(status)
