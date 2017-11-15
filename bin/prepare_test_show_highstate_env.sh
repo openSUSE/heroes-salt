fa45db18d3a30df4cd1d1a67009af7aa61b4bf3f
Theo Chatzimichos <tampakrap@gmail.com>
2017-11-15 17:09:51 +0100

    bin/prepare_test_show_highstate_env.sh: make the secrets stripping optional

--- bin/prepare_test_show_highstate_env.sh
+++ bin/prepare_test_show_highstate_env.sh
@@ -13,6 +13,25 @@ if [[ $(whoami) != 'root' ]]; then
     fi
 fi
 
+help() {
+    echo "Prepares the CI runner or workstation environment to run the show_highstate tests"
+    echo
+    echo "Arguments:"
+    echo
+    echo "-s    Strip out secrets files (CI runner can't read them)"
+    echo
+}
+
+[[ $1 == '--help' ]] && help && exit
+
+while getopts sh arg; do
+    case ${arg} in
+        s) STRIP_SECRETS=1 ;;
+        h) help && exit ;;
+        *) help && exit 1 ;;
+    esac
+done
+
 $SUDO zypper -qn in --no-recommends salt git python3 python3-PyYAML
 $SUDO rm -rf /srv/{salt,pillar}
 $SUDO ln -s $PWD/salt /srv/salt
@@ -22,4 +41,4 @@ ROLES=$(bin/get_roles.py --yaml)
 printf "city:\ncountry:\ndomain: infra.opensuse.org\nosfullname:\nosmajorrelease:\nosrelease_info:\n$ROLES\nsalt_cluster: opensuse\nvirt_cluster:" | $SUDO tee /etc/salt/grains > /dev/null
 sed -i -e 's/^production:$/base:/' /srv/{salt,pillar}/top.sls
 touch pillar/id/${ID//./_}.sls
-sed -i -e "s#\- secrets\..*#- id.${ID//./_}#g" $(grep -lr "\- secrets\.")
+[[ -n $STRIP_SECRETS ]] && sed -i -e "s#\- secrets\..*#- id.${ID//./_}#g" $(grep -lr "\- secrets\." pillar)
