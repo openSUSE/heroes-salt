f1d227a06cce987cf63ed10ded11edc35b2a2d5b
Theo Chatzimichos <tampakrap@opensuse.org>
2018-01-19 17:05:07 +0100

    bin/test_roles.py: there is no reason to open the */role/*.sls files

--- bin/test_roles.py
+++ bin/test_roles.py
@@ -16,10 +16,9 @@ roles = get_roles(append=['base', 'web'])
 for directory in ['salt', 'pillar']:
     for sls in os.listdir('%s/role' % directory):
         if sls.endswith('.sls'):
-            with open('%s/role/%s' % (directory, sls)) as f:
-                if sls.split('.sls')[0] not in roles:
-                    print('%s/role/%s not in roles' % (directory, sls))
-                    status = 1
+            if sls.split('.sls')[0] not in roles:
+                print('%s/role/%s not in roles' % (directory, sls))
+                status = 1
 
 for role in roles:
     if not os.path.isfile('salt/role/%s.sls' % role):
