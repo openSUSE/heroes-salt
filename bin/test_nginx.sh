8f3b0f756215f72408599687c79d34885174821a
Theo Chatzimichos <tampakrap@opensuse.org>
2018-01-20 20:29:04 +0100

    use fake cert/key pair for testing nginx configs
    We are replacing both the cert/key pair because:
    - the key is encrypted and the CI worker can't decrypt it
    - the nginx validation command tries to match the pair

--- bin/test_nginx.sh
+++ bin/test_nginx.sh
@@ -16,6 +16,23 @@ reset_nginx() {
     printf "roles:\n- $role" | $SUDO tee /etc/salt/grains > /dev/null
 }
 
+create_fake_certs() {
+    # We are replacing both the cert/key pair because:
+    # - the key is encrypted and the CI worker can't decrypt it
+    # - the nginx validation command tries to match the pair
+
+    PRIVATE_KEYS=( $(grep ssl_certificate_key pillar/role/$role.sls | cut -d':' -f2) )
+    for key in ${PRIVATE_KEYS[@]}; do
+        $SUDO cp test/fixtures/domain.key $key
+    done
+
+    PUBLIC_CERTS=( $(grep "ssl_certificate:" pillar/role/$role.sls | cut -d':' -f2) )
+    for cert in ${PUBLIC_CERTS[@]}; do
+        $SUDO cp test/fixtures/domain.crt $cert
+    done
+}
+
+
 WEB_ROLES=( $(bin/get_roles.py | grep web_) )
 
 for role in ${WEB_ROLES[@]}; do
@@ -23,6 +40,7 @@ for role in ${WEB_ROLES[@]}; do
         echo "Testing role: $role"
         reset_nginx
         $SUDO salt-call --local -l quiet state.apply role.$role > /dev/null
+        create_fake_certs
         if $(nginx -tq); then
             echo 'PASSED'
         else
