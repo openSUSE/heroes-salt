c8a26d1942ea68e4943b66ef8f3ab016a7d5b55c
Christian Boltz <opensuse@cboltz.de>
2018-05-05 20:49:37 +0200

    Wiki: allow upload of *.svg files
    Fixes https://github.com/openSUSE/wiki/issues/18

--- salt/profile/wiki/files/LocalSettings.php
+++ salt/profile/wiki/files/LocalSettings.php
@@ -194,7 +194,7 @@ $wgNamespacesToBeSearchedDefault = array(
 $wgUseAjax = true; // Enable Ajax
 $wgAllowExternalImages = true; // Enable links to external images
 # Allow upload of files with the following extensions
-$wgFileExtensions = array( 'doc', 'docx', 'gif', 'jpg', 'jpeg', 'odp', 'ods', 'odt', 'pdf', 'png', 'ppt', 'pptx', 'sxc', 'sxw', 'xls', 'xlsx' );
+$wgFileExtensions = array( 'doc', 'docx', 'gif', 'jpg', 'jpeg', 'odp', 'ods', 'odt', 'pdf', 'png', 'ppt', 'pptx', 'svg', 'sxc', 'sxw', 'xls', 'xlsx' );
 # Add XMPP functionality
 $wgUrlProtocols[] = 'xmpp:';
 
