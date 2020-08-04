2ee807ad75363e147f10d601d7b14a269595a22e
Stasiek Michalski <hellcp@opensuse.org>
2020-08-04 07:22:32 +0000

    Change the urls to better reflect the function

--- salt/profile/mailman3/files/urls.py
+++ salt/profile/mailman3/files/urls.py
@@ -24,9 +24,9 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(
-        url=reverse_lazy('list_index'),
+        url=reverse_lazy('hk_root'),
         permanent=True)),
-    url(r'^mailman3/', include('postorius.urls')),
+    url(r'^manage/', include('postorius.urls')),
     url(r'^archives/', include('hyperkitty.urls')),
     url(r'', include('django_mailman3.urls')),
     url(r'^accounts/', include('allauth.urls')),
