c94236d4afdf5f067b32148fe18f1d82197f285b
Olav Reinert <oreinert@opensuse.org>
2021-04-17 12:00:02 +0000

    new-forum: various minor enhancements

--- salt/profile/vbulletin/files/db-tweak.sql
+++ salt/profile/vbulletin/files/db-tweak.sql
@@ -1,6 +1,8 @@
-grant all on '{{dbname}}'.* to '{{username}}'@'{{host}}' identified by '{{password}}';
+grant all on {{dbname}}.* to '{{username}}'@'{{host}}' identified by '{{password}}';
 
 update vb_setting set value='{{bburl}}' where varname='bburl';
 update vb_setting set value='{{frontendurl}}' where varname='frontendurl';
 
 update vb_setting set value=1 where varname='bburl_basepath';
+update vb_setting set value='/srv/www/vhosts/forums/sitemap' where varname='sitemap_path';
+
