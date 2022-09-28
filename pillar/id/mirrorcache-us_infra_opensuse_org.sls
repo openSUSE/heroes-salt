openSUSE/heroes-salt

commit d3ac508b893bb80c6757af7a4baa549b0ac7e79c

Add ssh keys for mirrorcache
this introduces one level of indirection,
so that keys do not have to be managed per host

pillar/id/mirrorcache-us_infra_opensuse_org.sls
@@ -1,3 +1,6 @@
+include:
+  ssh_keys.groups.download_infra
+
 grains:
   city: provo
   country: us

pillar/id/mirrorcache2_infra_opensuse_org.sls
@@ -1,3 +1,6 @@
+include:
+  ssh_keys.groups.download_infra
+
 grains:
   city: nuremberg
   country: de

pillar/id/mirrorcache_infra_opensuse_org.sls
@@ -1,3 +1,6 @@
+include:
+  ssh_keys.groups.download_infra
+
 grains:
   city: nuremberg
   country: de

pillar/ssh_keys/groups/download_infra.sls
@@ -1,0 +1,11 @@
+# This is the key group for people managing download infra
+# such as mirrorcache*
+
+include:
+  - ssh_keys.users.anikitin
+  - ssh_keys.users.bmwiedemann
+  - ssh_keys.users.darix
+  - ssh_keys.users.eroca
+  - ssh_keys.users.gpfuetzenreuther
+  - ssh_keys.users.mstrigl
+  - ssh_keys.users.ro

pillar/ssh_keys/users/anikitin.sls
@@ -1,0 +1,4 @@
+users:
+  root:
+    ssh_auth_file:
+      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmOWk3/JHBwygczVGBWODCVjNMMWugYIYKRs6a65zVL1XbVg2LSGnTMk5Afa+7hDq1YZEU6D+yKY8FQtWbLcNc75yGorhrKMUd3bV6vEf3OlfRBMUhfNgoqHt0gOv/Os/ox/SxoZtGqdFqV58KOlix1KTC8o6H1oSmz99Z5KKtjIi7fFiHq6/rgg5ulOdRxQ9LoEJ6dMQIMIE3LZz6x/1MW7bfVg8ijN58zyo4j5s4p/Jiwfj/XZ47GBWScfI8bZNYHuQrj2U3F5hqQMNiStjLnZ/huOkPIEIDyY+MeNv2nG0DQTlPNG5xVVTjnuLQMlAjw5bdJqYDbx1ONGhHElSh anikitin@suse.de Andrii Nikitin

pillar/ssh_keys/users/bmwiedemann.sls
@@ -1,0 +1,4 @@
+users:
+  root:
+    ssh_auth_file:
+      - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMI/OukYJONiHQqMIEMJQZd4TS9jB6+1q+jzm6LBwv18 bwiedemann@bernhard.suse.de Bernhard M. Wiedemann

pillar/ssh_keys/users/darix.sls
@@ -1,0 +1,5 @@
+users:
+  root:
+    ssh_auth_file:
+      - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIz54hFXvTbQ3ZMdQoYb6J0tbhTzk1dTcWA5WhSH1mqy darix@oriza  Marcus Rueckert
+      - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILfzEDxkpk0cagxG10Ta3mlqi+S4mqBOUPjkCOx8m/jT darix@mei.nordisch.org

pillar/ssh_keys/users/eroca.sls
@@ -1,0 +1,4 @@
+users:
+  root:
+    ssh_auth_file:
+      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+XFB6tA0dQwR/ItEyAgtupDpeXyG91nKvYxiZogSBa+hJdjeOG/skrBOCtN1E7l6jOtlcbHKQhYclublgWukkZ4PL5XRu3SWwdAxY3YtFD3d+sJtr0w5UcP4nAfkDzrIICxUZA11GPtA93J/9/VL2RdRhJmYdYlcx2MlkbmzQAZ3md+AeFMbU6C7woA0lw2TnBN6jtqe4QDNCGcc2b0scqu0L1k/Jzdya7nhoRKX5j4ev7xVhFE9fCZDBGYcyy3qb2NLMomOJOHotHhGlu4xxcooJgsAHM554B1gMVYGoaG4QQdrtEZnQ8kZhHuWLqW4gF8hi8qNUM+IobYI772Kt eroca Elisei Roca

commit fae8bd99e528672761c9fe204e4ffafa859d2e1c

Fixup mirrorcache sls format
broken from d3ac508b893bb80c6757af7a4baa549b0ac7e79c

pillar/id/mirrorcache-us_infra_opensuse_org.sls
@@ -1,5 +1,5 @@
 include:
-  ssh_keys.groups.download_infra
+  - ssh_keys.groups.download_infra
 
 grains:
   city: provo

pillar/id/mirrorcache2_infra_opensuse_org.sls
@@ -1,5 +1,5 @@
 include:
-  ssh_keys.groups.download_infra
+  - ssh_keys.groups.download_infra
 
 grains:
   city: nuremberg

pillar/id/mirrorcache_infra_opensuse_org.sls
@@ -1,5 +1,5 @@
 include:
-  ssh_keys.groups.download_infra
+  - ssh_keys.groups.download_infra
 
 grains:
   city: nuremberg