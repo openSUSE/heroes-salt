d8e613db5aca334b00442915667ea948f293b539
Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
2023-05-13 17:12:03 +0200

    Import replace_secrets.sh
    This copies my replace_secrets script from OPS-Service/salt.
    
    Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

---
+++ bin/replace_secrets.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# This will replace all PGP messages in the pillar with the value defined as $substitute. This is handy to avoid "Could not decrypt" errors during local testing.
+# Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
+
+substitute="SUBSTITUTED"
+searchpath="pillar/secrets/"
+
+find "$searchpath" -type f -name '*.sls' -execdir \
+	gawk -i inplace -v subst="$substitute" \
+	'{if (FNR==1 && !/#!yaml\|gpg/ && !/#!gpg\|yaml/ && !/# empty/){ nextfile }}; !/\|$/{ORS="\n"}; /\|$/{ORS=""; gsub(/\|$/, "")}; /BEGIN PGP MESSAGE/{f=1} !f; /END PGP MESSAGE/{f=0; printf "%s\n", subst};' \
+	{} +
