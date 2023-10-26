8775f65f59c5a54511f173009d39bb74e7c80f0f
Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
2023-10-26 22:08:47 +0200

    Add convert_4to6 helper function
    This adds a convenience function for translating an IPv4 address to
    its IPv6 embedded counterpart.
    
    Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

--- salt/_modules/os_network.py
+++ salt/_modules/os_network.py
@@ -1,3 +1,19 @@
+import ipaddress
+
 def gw_with_cidr(gw, net):
     network, slash, cidr = net.partition('/')
     return f'{gw}{slash}{cidr}'
+
+def convert_4to6(v4address, v6prefix):
+    """
+    Example arguments:
+    v6prefix = 2a07:de40:b27e:64:: or 2a07:de40:b27e:64::/96
+    v4address = 192.168.47.37
+    Example return string:
+    2a07:de40:b27e:0064:0000:0000:c0a8:2f25
+    """
+
+    ip4address = ipaddress.IPv4Address(v4address)
+    ip6prefix = ipaddress.IPv6Address(v6prefix.split('/')[0])
+
+    return str(ipaddress.ip_address(int(ip4address) + int(ip6prefix)))
