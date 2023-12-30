0cb66a44d09775301f7db3b8b9706696661ba301
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2023-12-30 22:36:27 +0100

    test_nginx: render one job per role
    Instead of testing one nginx related role at a time, generate
    individual CI jobs to facilitate parallel execution.
    
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

--- bin/test_nginx.sh
+++ bin/test_nginx.sh
@@ -6,31 +6,13 @@ rpm -qa --qf '%{name}\n' | sort > /tmp/packages-before
 
 [[ $(whoami) == 'root' ]] || { echo 'Please run this script as root'; exit 1; }
 
-# systemctl refuses to work in a container, but is needed by service.running. Replace it with /usr/bin/true to avoid useless error messages
+# using a container without systemd, but systemd is needed by service.running. replace it with /usr/bin/true to avoid useless error messages.
 ( cd /usr/bin/ ; ln -sf true systemctl )
 
 source bin/get_colors.sh
 
-rpm -q nginx salt salt-master
-
-IDFILE="pillar/id/$(hostname).sls"
-IDFILE_BASE="$IDFILE.base.sls"
-sed -i -e '/virtual/d' /etc/salt/grains
-cp "$IDFILE" "$IDFILE_BASE"
-
-reset_nginx() {
-    cp "$IDFILE_BASE" "$IDFILE"
-    rm -rf /etc/nginx
-    cp -a /etc/nginx_orig /etc/nginx
-    printf "roles:\n- $role" >> "$IDFILE"
-}
-
-reset_ip() {
-    # Reset the grains-retrieved IPs to 127.0.0.1, as `nginx -t` actually tries
-    # to bind to any configured listen IP
-
-    sed -i -e "s/{{ ip4_.* }}/127.0.0.1/g" pillar/role/$role.sls
-}
+role="$1"
+IDFILE="pillar/id/$HOSTNAME.sls"
 
 create_fake_certs() {
     # We are replacing both the cert/key pair because:
@@ -59,7 +41,7 @@ create_fake_certs() {
 }
 
 touch_includes() {
-    case "$1" in
+    case "$role" in
         mailman3)
             touch /etc/nginx/mails.rewritemap
             touch /etc/nginx/lists.rewritemap
@@ -77,80 +59,79 @@ touch_includes() {
     esac;
 }
 
-cp -a /etc/nginx /etc/nginx_orig
+rolestatus=0
+sls_role="salt/role/${role/./\/}.sls"
+out="$role.txt"
+echo "START OF $role" > "$out"
+echo_INFO "Testing role: $role"
 
-for role in $(bin/get_roles.py); do
-    rolestatus=0
-    sls_role="salt/role/${role/./\/}.sls"
-    out="$role.txt"
-    if grep nginx "$sls_role" > /dev/null; then
-        echo "START OF $role" > "$out"
-        echo_INFO "Testing role: $role"
-        reset_nginx
-        reset_ip
-        if grep -q profile "$sls_role"
+printf "roles:\n- $role" >> "$IDFILE"
+
+# Reset the grains-retrieved IPs to 127.0.0.1, as `nginx -t` actually tries
+# to bind to any configured listen IP
+sed -i -e "s/{{ ip4_.* }}/127.0.0.1/g" pillar/role/$role.sls
+
+if grep -q profile "$sls_role"
+then
+    #for profile in "$(grep -h '\- profile' $sls_role | yq -o t)" // to-do: add yq to container
+    for profile in $(grep -h '\- profile' $sls_role | sed 's/^\s\+ -//' | tr '\n' ' ')
+    do
+        if [ ! "$profile" == 'profile.web.server.nginx' ]
         then
-            #for profile in "$(grep -h '\- profile' $sls_role | yq -o t)" // to-do: add yq to container
-            for profile in $(grep -h '\- profile' $sls_role | sed 's/^\s\+ -//' | tr '\n' ' ')
-            do
-                if [ ! "$profile" == 'profile.web.server.nginx' ]
-                then
-                    dir_profile="${profile//./\/}"
-                    if [ -f "salt/$dir_profile/nginx.sls" ]
-                    then
-                        state="$profile.nginx"
-                    elif [ -f "salt/${dir_profile%/*}/nginx.sls" ]
-                    then
-                        state="${profile%.*}.nginx"
-                    fi
-                    if [ -n "$state" ]
-                    then
-                        echo "Applying $state ..." >> "$out"
-                        salt-call --local state.apply "$state" >> "$out" || rolestatus=1
-                        echo >> "$out"
-                        unset state
-                        break
-                    fi
-                fi
-            done
+            dir_profile="${profile//./\/}"
+            if [ -f "salt/$dir_profile/nginx.sls" ]
+            then
+                state="$profile.nginx"
+            elif [ -f "salt/${dir_profile%/*}/nginx.sls" ]
+            then
+                state="${profile%.*}.nginx"
+            fi
+            if [ -n "$state" ]
+            then
+                echo "Applying $state ..." >> "$out"
+                salt-call --local state.apply "$state" >> "$out" || rolestatus=1
+                echo >> "$out"
+                unset state
+                break
+            fi
         fi
-        echo 'Applying nginx ...' >> "$out"
-        salt-call --local state.apply nginx >> "$out" || rolestatus=1
-        create_fake_certs
-        touch_includes $role
+    done
+fi
 
-        printf '\nTesting configuration ...\n' >> "$out"
-        mispipe 'nginx -tq' "tee -a $out" || rolestatus=1
+echo 'Applying nginx ...' >> "$out"
+salt-call --local state.apply nginx >> "$out" || rolestatus=1
+create_fake_certs
+touch_includes $role
 
-        printf '\nDumping configuration ...\n' >> "$out"
-        nginx -T >> "$out"
+printf '\nTesting configuration ...\n' >> "$out"
+mispipe 'nginx -tq' "tee -a $out" || rolestatus=1
 
-        # make sure all vhost config files are named *.conf (without that suffix, they get ignored)
-        for file in /etc/nginx/vhosts.d/* ; do
-            test "$file" == "/etc/nginx/vhosts.d/*" && continue  # skip loop if no file exists in vhosts.d/
-            echo "$file" | grep -q '\.conf$' || {
-                echo "ERROR: $file is not named *.conf"
-                rolestatus=1
-            }
-        done
+printf '\nDumping configuration ...\n' >> "$out"
+nginx -T >> "$out"
 
-        if test $rolestatus = 0; then
-            echo_PASSED
-        else
-            echo_FAILED
-            head -n1000 /etc/nginx/vhosts.d/*
-            echo "### end of /etc/nginx/vhosts.d/* for role $role"
-            STATUS=1
-        fi
-        echo
-        echo "END OF $role" >> "$out"
-    fi
+# make sure all vhost config files are named *.conf (without that suffix, they get ignored)
+for file in /etc/nginx/vhosts.d/* ; do
+    test "$file" == "/etc/nginx/vhosts.d/*" && continue  # skip loop if no file exists in vhosts.d/
+    echo "$file" | grep -q '\.conf$' || {
+        echo "ERROR: $file is not named *.conf"
+        rolestatus=1
+    }
 done
+
+if test $rolestatus = 0; then
+    echo_PASSED
+else
+    echo_FAILED
+    head -n1000 /etc/nginx/vhosts.d/*
+    echo "### end of /etc/nginx/vhosts.d/* for role $role"
+    STATUS=1
+fi
+echo
+echo "END OF $role" >> "$out"
 
 rpm -qa --qf '%{name}\n' | sort > /tmp/packages-after
 
-diff -U0 /tmp/packages-before /tmp/packages-after || echo '=== The packages listed above were installed by one of the roles. Consider to add them to the docker image to speed up this test.'
-
+diff -U0 /tmp/packages-before /tmp/packages-after || echo '=== The packages listed above were installed by one of the roles. Consider to add them to the container image to speed up this test.'
 
 exit $STATUS
 
