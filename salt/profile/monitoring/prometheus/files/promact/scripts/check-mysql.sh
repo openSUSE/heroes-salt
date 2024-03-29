d8214ec4d37e6f86e351f76120e3f4aa23034141
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2024-03-29 14:57:17 +0100

    Enable templating in Promact states
    - render variables in configuration instead of plainly printing them
    - add common header to script
    
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

--- salt/profile/monitoring/prometheus/files/promact/scripts/check-mysql.sh
+++ salt/profile/monitoring/prometheus/files/promact/scripts/check-mysql.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -eu
+#{{ pillar['managed_by_salt'] }}
+
 mariadb-admin --defaults-file=/etc/opt/promact/my.cnf --connect-timeout=10 \
   -h "$INSTANCE" -u monitor processlist > /srv/monitor-internal/promact-out/mysql_processlist_"$INSTANCE".txt
