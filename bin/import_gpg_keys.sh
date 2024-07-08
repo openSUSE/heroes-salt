923bd6acd9d657f188237a9d5531470cb94afde2
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2024-07-08 21:47:39 +0200

    import_gpg_keys: remove obsolete advice
    We no longer do bogus trusting of PGP keys.
    
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

--- bin/import_gpg_keys.sh
+++ bin/import_gpg_keys.sh
@@ -4,7 +4,7 @@ set -eu
 shopt -s nullglob
 
 # Imports the other admins' plus the salt master/syndic's GPG keys into the
-# local keyring, and opens the trust menu in order to trust them ultimately
+# local keyring
 
 RECIPIENTS=( $(grep -E '^\s*0x' encrypted_pillar_recipients) )
 
