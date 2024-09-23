openSUSE/heroes-salt

commit 8e7bf5e59e776b3b6f93872ffdbc8509e5be3fe4

Narrow down IP address for LLDP
The address would fluctuate between the GUA and the ULA address behind
the FQDN depending on which happened to be first in the list during a
state.apply. We always want it to be the ULA one, hence filter for it.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/common/lldp.sls
@@ -1,7 +1,9 @@
+{%- set addresses = grains['fqdn_ip6'] | ipaddr('private') %}
+
 lldpd:
   sysconfig:
     lldpd_options: >-
       -C !*
       -I fib*,free*,mgmt*,ob*
-      -m {{ grains['fqdn_ip6'][0] }}
+      {{ '-m ' ~ addresses | first if addresses else '' }}
       -M 1

commit 611f19b8abc0a5b06299dbfbcd5c4cedde7baa75

Repair LLDP argument formatting on no address
Do not insert an empty line into the argument list if no address is
present.

Fixes: 8e7bf5e59e77 ("Narrow down IP address for LLDP")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/common/lldp.sls
@@ -5,5 +5,4 @@ lldpd:
     lldpd_options: >-
       -C !*
       -I fib*,free*,mgmt*,ob*
-      {{ '-m ' ~ addresses | first if addresses else '' }}
-      -M 1
+      {{ '-m ' ~ addresses | first if addresses else '' }} -M1