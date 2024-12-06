openSUSE/heroes-salt

commit 235ff5a22c25a5b120e6076a5bc1e47359772597

Add link between slc-mirror and debuginfod
debuginfod needs fast access to RPMs on slc-mirror, add backchannel
link to benefit of unrouted 100G throughput and jumbo frames - traffic
will be limited to NFS.

Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

pillar/cluster/arches/network.sls
@@ -40,6 +40,9 @@ network:
     -%}
     {{ vlantapnetworks(vlanlist_r, 'bond-fib', 'slc1', 1500) }}
 
+    # VLAN interfaces for unrouted VM connectivity
+    {{ vlantap('os-mirror-bc', 1708, 'bond-fib', 9216) }}
+
   {{ default_gateway('slc1', 'openSUSE-bare') }}
 
 firewalld:

pillar/id/debuginfod_infra_opensuse_org.sls
@@ -13,3 +13,8 @@ grains:
   weburls: []
 roles:
   - debuginfod
+
+network:
+  interfaces:
+    os-mirror-bc:
+      mtu: 9216

pillar/id/slc-mirror_infra_opensuse_org.sls
@@ -21,9 +21,19 @@ firewalld:
           - 873
           - 874
   zones:
+    backchannel:
+      interfaces:
+        - os-mirror-bc
+      services:
+        - nfs
     internal:
       interfaces:
         - os-mirror
       services:
         - http
         - rsync-mirror
+
+network:
+  interfaces:
+    os-mirror-bc:
+      mtu: 9216

pillar/infra/hosts.yaml
@@ -172,6 +172,10 @@ debuginfod:
       ip6: 2a07:de40:617e:1907::a/64
       mac: 00:16:3e:66:e1:fa
       source: x-os-dbginfod
+    os-mirror-bc:
+      ip6: fd4d:665a:d688:1708::100/64
+      mac: 00:16:3e:66:e1:fb
+      source: x-os-mirror-bc
   ram: 32768MB
   vcpu: 8
 devcon:
@@ -1008,6 +1012,10 @@ slc-mirror:
       ip6: 2a07:de40:617e:1905::a/64
       mac: 00:16:3e:00:16:6a
       source: x-os-mirror
+    os-mirror-bc:
+      ip6: fd4d:665a:d688:1708::a/64
+      mac: 00:16:3e:00:16:6b
+      source: x-os-mirror-bc
   legacy_boot: true
   ram: 8192MB
   vcpu: 4

pillar/role/debuginfod.sls
@@ -9,6 +9,9 @@ firewalld:
         tcp:
           - 8002
   zones:
+    backchannel:
+      interfaces:
+        - os-mirror-bc
     internal:
       interfaces:
         - os-dbginfod

commit 72a4953f10037009c5d5d76c7362e36f1033561a

Import vlantap macro in Arches pillar
Macro was referenced but forgotten to be imported.

Fixes: 235ff5a22c25 ("Add link between slc-mirror and debuginfod")
Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

pillar/cluster/arches/network.sls
@@ -1,4 +1,4 @@
-{%- from 'macros.jinja' import bond, default_gateway, slave, vlantapnetworks %}
+{%- from 'macros.jinja' import bond, default_gateway, slave, vlantap, vlantapnetworks %}
 
 network:
   interfaces: