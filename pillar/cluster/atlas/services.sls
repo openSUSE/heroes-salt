openSUSE/heroes-salt

commit d62fb20e9bbb090651d74a4d402c1603b1244c21

Match blacklisted user agents case insensitively
Additional matching success without having to add more paterns to the
file.

Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

pillar/cluster/atlas/services.sls
@@ -219,7 +219,7 @@ haproxy:
       acls:                              # daffy1              # daffy2
         - src_login         src          2a07:de40:b280:86::11 2a07:de40:b280:86::12
         - annoying_networks   req.hdr_ip(X-Forwarded-For)  -f /etc/haproxy/blacklists/networks -n
-        - annoying_useragents hdr_sub(User-Agent)          -f /etc/haproxy/blacklists/useragents
+        - annoying_useragents hdr_sub(User-Agent)          -f /etc/haproxy/blacklists/useragents -i
         - odd_clients         req.hdr_cnt(Accept-Language) 0
 
         - path_indexphp     path_beg     /index.php
@@ -248,7 +248,7 @@ haproxy:
     http-misc:
       acls:
         - annoying_networks   src                  -f /etc/haproxy/blacklists/networks -n
-        - annoying_useragents hdr_sub(User-Agent)  -f /etc/haproxy/blacklists/useragents
+        - annoying_useragents hdr_sub(User-Agent)  -f /etc/haproxy/blacklists/useragents -i
         - is_ssl              dst_port    443
 
         {%- for host_pagure in ['code', 'pages', 'ev', 'releases'] %}

pillar/cluster/common/public_proxy.sls
@@ -4,8 +4,8 @@ haproxy:
   frontends:
     http:
       acls:
-        - annoying_networks   src                  -f /etc/haproxy/blacklists/networks -n  # salt/profile/proxy/files/etc/haproxy/blacklists/networks
-        - annoying_useragents hdr_sub(User-Agent)  -f /etc/haproxy/blacklists/useragents   # salt/profile/proxy/files/etc/haproxy/blacklists/useragents
+        - annoying_networks   src                  -f /etc/haproxy/blacklists/networks -n    # salt/profile/proxy/files/etc/haproxy/blacklists/networks
+        - annoying_useragents hdr_sub(User-Agent)  -f /etc/haproxy/blacklists/useragents -i  # salt/profile/proxy/files/etc/haproxy/blacklists/useragents
         - is_ssl              dst_port    443
       options:
         - http-server-close

commit 06e64af4132733c6df897db998cf5b735ea3b110

Correct HAProxy user agent ACLs
The "-i" flag for case insensitive matching only applies to "-f" file
lists passed _after_ it. This is documented behavior but was missed.

Fixes: d62fb20e9bbb ("Match blacklisted user agents case insensitively")
Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

pillar/cluster/atlas/services.sls
@@ -219,7 +219,7 @@ haproxy:
       acls:                              # daffy1              # daffy2
         - src_login         src          2a07:de40:b280:86::11 2a07:de40:b280:86::12
         - annoying_networks   req.hdr_ip(X-Forwarded-For)  -f /etc/haproxy/blacklists/networks -n
-        - annoying_useragents hdr_sub(User-Agent)          -f /etc/haproxy/blacklists/useragents -i
+        - annoying_useragents hdr_sub(User-Agent)          -i -f /etc/haproxy/blacklists/useragents
         - odd_clients         req.hdr_cnt(Accept-Language) 0
 
         - path_indexphp     path_beg     /index.php
@@ -248,7 +248,7 @@ haproxy:
     http-misc:
       acls:
         - annoying_networks   src                  -f /etc/haproxy/blacklists/networks -n
-        - annoying_useragents hdr_sub(User-Agent)  -f /etc/haproxy/blacklists/useragents -i
+        - annoying_useragents hdr_sub(User-Agent)  -i -f /etc/haproxy/blacklists/useragents
         - is_ssl              dst_port    443
 
         {%- for host_pagure in ['code', 'pages', 'ev', 'releases'] %}

pillar/cluster/common/public_proxy.sls
@@ -5,7 +5,7 @@ haproxy:
     http:
       acls:
         - annoying_networks   src                  -f /etc/haproxy/blacklists/networks -n    # salt/profile/proxy/files/etc/haproxy/blacklists/networks
-        - annoying_useragents hdr_sub(User-Agent)  -f /etc/haproxy/blacklists/useragents -i  # salt/profile/proxy/files/etc/haproxy/blacklists/useragents
+        - annoying_useragents hdr_sub(User-Agent)  -i -f /etc/haproxy/blacklists/useragents  # salt/profile/proxy/files/etc/haproxy/blacklists/useragents
         - is_ssl              dst_port    443
       options:
         - http-server-close