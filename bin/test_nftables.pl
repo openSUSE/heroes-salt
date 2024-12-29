openSUSE/heroes-salt

commit ab109e18783202cb324aa9bc4a52291f71276812

Asgard: first nftables refactoring
First effort to make use of Jinja templating to deduplicate and
reduce code in our nftables tree.
There is still more to do, and some of the functionality should be
moved to macros, but the aim is to do smaller batches as rendered output
is formatted slightly differently, requiring careful review upon
application.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/files/nftables/asgard/base_inet/forward.nft
@@ -2,47 +2,30 @@
 ## MANAGED BY SALT in salt/files/nftables/asgard ##
 ###################################################
 
+{%- import_yaml 'pillar/infra/networks.yaml' as networks %}
+{%- set pseudo_networks = networks.pop('pseudo') %}
+{%- set chainmap = {
+          'iif': 'input',
+          'oif': 'output',
+        }
+%}
+
  chain forward {
   type filter hook forward priority 0
   policy drop
   jump global
 
-  iif vmap {
-    os-asgard   : jump network_os-asgard,
-    os-bare     : jump input_network_os-bare,
-    os-internal : jump input_network_os-internal,
-    os-log      : jump input_network_os-log,
-    os-s-warp   : jump input_network_os-s-warp,
-    os-p2p-pub  : jump input_network_os-p2p-pub,
-    os-mirror   : jump input_network_os-mirror,
-    os-salt     : jump input_network_os-salt,
-    os-public   : jump input_network_os-public,
-    os-thor     : jump input_network_os-thor,
-    os-code     : jump input_network_os-code,
-    os-ghr-c    : jump input_network_os-ghr-c,
-    os-ghr-os   : jump input_network_os-ghr-os,
-    os-odin     : jump input_network_os-odin,
-    os-kani     : jump input_network_os-kani,
-    os-netbox   : jump input_network_os-netbox,
-    os-mail     : jump input_network_os-mail,
+{% for x in ['iif', 'oif'] %}
+  {{ x }} vmap {
+    {%- if x == 'iif' %}
+    os-asgard                      : jump network_os-asgard,
+    {%- endif %}
+    {%- for network, network_config in networks['prg2'].items() | sort(attribute='1.short') %}
+      {%- set shortnet = network_config['short'] %}
+    {{ shortnet }} {{ ( ': jump ' ~ chainmap[x] ~ '_network_' ~ shortnet ) | indent(30 - shortnet | length, true) }},
+    {%- endfor %}
   }
-  oif vmap {
-    os-bare     : jump output_network_os-bare,
-    os-internal : jump output_network_os-internal,
-    os-log      : jump output_network_os-log,
-    os-s-warp   : jump output_network_os-s-warp,
-    os-p2p-pub  : jump output_network_os-p2p-pub,
-    os-mirror   : jump output_network_os-mirror,
-    os-salt     : jump output_network_os-salt,
-    os-thor     : jump output_network_os-thor,
-    os-code     : jump output_network_os-code,
-    os-ghr-c    : jump output_network_os-ghr-c,
-    os-ghr-os   : jump output_network_os-ghr-os,
-    os-odin     : jump output_network_os-odin,
-    os-kani     : jump output_network_os-kani,
-    os-netbox   : jump output_network_os-netbox,
-    os-mail     : jump output_network_os-mail,
-  }
+{% endfor %}
 
   # WireGuard interfaces not using iif to prevent breakage on tunnel restarts
   iifname nueqsc_stonehat jump input_network_nueqsc

salt/files/nftables/asgard/base_inet/input.nft
@@ -2,6 +2,9 @@
 ## MANAGED BY SALT in salt/files/nftables/asgard ##
 ###################################################
 
+{%- import_yaml 'pillar/infra/networks.yaml' as networks %}
+{%- set pseudo_networks = networks.pop('pseudo') %}
+
  # input chain, this is for traffic targeted towards the local system (i.e. this firewall itself)
  chain input {
   type filter hook input priority 0
@@ -9,27 +12,15 @@
 
   jump global
 
+
   iif vmap {
-    os-asgard   : jump network_os-asgard,
-    os-s-warp   : jump input_network_os-s-warp,
-    os-internal : jump input_network_os-internal,
-    os-log      : jump input_network_os-log,
-    os-bare     : jump input_network_os-bare,
-    os-p2p-pub  : jump input_network_os-p2p-pub,
-    os-public   : jump input_network_os-public,
-    os-mirror   : jump input_network_os-mirror,
-    os-mirror   : jump input_network_os-mirror,
-    os-salt     : jump input_network_os-salt,
-    os-ipmi-r   : jump input_network_os-ipmi-r,
-    os-thor     : jump input_network_os-thor,
-    os-code     : jump input_network_os-code,
-    os-ghr-c    : jump input_network_os-ghr-c,
-    os-ghr-os   : jump input_network_os-ghr-os,
-    os-odin     : jump input_network_os-odin,
-    os-kani     : jump input_network_os-kani,
-    os-netbox   : jump input_network_os-netbox,
-    os-mail     : jump input_network_os-mail,
+    os-asgard                      : jump network_os-asgard,
+    {%- for network, network_config in networks['prg2'].items() | sort(attribute='1.short') %}
+      {%- set shortnet = network_config['short'] %}
+    {{ shortnet }} {{ ( ': jump input_network_' ~ shortnet ) | indent(30 - shortnet | length, true) }},
+    {%- endfor %}
   }
+
 
   # WireGuard interfaces not using iif to prevent breakage on tunnel restarts
   iifname nueqsc_stonehat jump input_network_nueqsc

salt/files/nftables/asgard/base_inet/sets.nft
@@ -7,20 +7,66 @@
 # 2. Addresses of this firewall cluster
 # 3. Addresses of other hosts
 
+{%- import_yaml 'pillar/infra/networks.yaml' as networks %}
+{%- set pseudo_networks = networks.pop('pseudo') %}
 
-set lan_ipv6 {
+{% for site, site_networks in networks.items() %}
+###
+#  BEGIN: {{ site | upper }}
+###
+    {%- for with_nat in [true, false] %}
+set lan_ipv6_{{ site }}{{ '' if with_nat else '_nonat' }} {
   type ipv6_addr
   flags interval
   auto-merge
-  elements = { $net6_os-bare, $net6_os-ipmi-r, $net6_os-internal, $net6_os-log, $net6_os-public, $net6_os-mirror, $net6_os-salt, $net6_os-thor, $nat64_v6, $net6_os-code, $net6_os-ghr-c, $net6_os-ghr-os, $net6_os-odin, $net6_os-kani, $net6_os-netbox, $net6_os-mail, $host6_qsc-ns3, $host6_slimhat }
+  elements = {
+    {%- for network, network_config in site_networks.items() %}
+    {{ network_config['net6'] }},
+    {%- endfor %}
+    {%- if site == 'prg2' %}
+    $host6_qsc-ns3,
+    $host6_slimhat,
+    {%- endif %}
+    {%- if with_nat %}
+    {{ pseudo_networks[site]['openSUSE-NAT64-Pool']['net6'] }},
+    {%- endif %}
+  }
 }
-
-set lan_ipv6_nonat {
+    {%- endfor %}
+{{ '' }}
+{%- if site == 'prg2' %}
+  {%- for network, network_config in site_networks.items() %}
+# VLAN {{ network_config['id'] }} ({{ network }})
+    {%- for i in [6, 4] %}
+      {%- set net = 'net' ~ i | string %}
+      {%- if net in network_config %}
+set {{ net }}_{{ network_config['short'] }} {
+  type ipv{{ i }}_addr
+  flags interval
+  elements = {
+    {{ network_config[net] }}
+  }
+}
+    {%- endif %}  {#- close IP family check #}
+   {%- endfor %}  {#- close IP family loop #}
+set self6_{{ network_config['short'].replace('os-', '') }} {
   type ipv6_addr
   flags interval
-  auto-merge
-  elements = { $net6_os-bare, $net6_os-ipmi-r, $net6_os-internal, $net6_os-log, $net6_os-public, $net6_os-mirror, $net6_os-salt, $net6_os-thor, $net6_os-odin, $net6_os-code, $net6_os-ghr-c, $net6_os-ghr-os, $net6_os-kani, $net6_os-netbox, $net6_os-mail, $host6_qsc-ns3, $host6_slimhat }
+  elements = {
+    {%- set gw6 = network_config['gw6'] %}
+    {%- if gw6.endswith('::3') %}
+    {{ gw6[:-1] }}1/128,
+    {{ gw6[:-1] }}2/128,
+    {%- endif %}
+    {{ gw6 }}/128,
+  }
 }
+  {%- endfor %}   {#- close site_networks loop #}
+{%- endif %}      {#- close site check #}
+###
+#  END: {{ site | upper }}
+###
+{% endfor %}
 
 set vpn_ipv6 {
     type ipv6_addr
@@ -32,25 +78,7 @@ set vpn_ipv6 {
     }
 }
 
-set lan_ipv6_slc1 {
-    type ipv6_addr
-    flags interval
-    auto-merge
-    elements = {
-      2a07:de40:617e:1800::/64,  # bare
-      2a07:de40:617e:1801::/64,  # devcon
-      2a07:de40:617e:1802::/64,  # sif
-      2a07:de40:617e:1803::/64,  # s-warp
-      2a07:de40:617e:1900::/64,  # salt
-      2a07:de40:617e:1903::/64,  # internal
-      2a07:de40:617e:1904::/64,  # public
-      2a07:de40:617e:1905::/64,  # mirror
-      2a07:de40:617e:1906::/64,  # mircache
-      2a07:de40:617e:1907::/64,  # os-dbginfod
-   }
-}
-
-## Networks
+## Additional networks
 
 # VLAN 3201 (openSUSE-SUSE-p2p-pub)
 set net6_os-p2p-pub {
@@ -74,92 +102,6 @@ set net6_os-asgard {
   flags interval
   elements = {
     fd4b:5292:d67e:1000::/64
-  }
-}
-
-# VLAN 1100 openSUSE-thor
-set net6_os-thor {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1100::/64
-  }
-}
-
-# VLAN 1101 openSUSE-SUSE-warp
-set net6_os-s-warp {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1101::/64
-  }
-}
-
-# VLAN 1200 openSUSE-salt
-set net6_os-s-salt {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1200::/64
-  }
-}
-
-# VLAN 1201 openSUSE-bare
-set net6_os-bare {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1201::/64
-  }
-}
-
-# VLAN 1202 openSUSE-IPMI-r
-set net6_os-ipmi-r {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1202::/64
-  }
-}
-
-# VLAN 1203 openSUSE-internal
-set net6_os-internal {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1203::/64
-  }
-}
-set net4_os-internal {
-  type ipv4_addr
-  flags interval
-  elements = {
-    172.16.129.0/24
-  }
-}
-
-# VLAN 1204 openSUSE-public
-set net6_os-public {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1204::/64
-  }
-}
-set net4_os-public {
-  type ipv4_addr
-  flags interval
-  elements = {
-    172.16.130.0/24
-  }
-}
-
-# VLAN 1205 openSUSE-mirror
-set net6_os-mirror {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1205::/64
   }
 }
 
@@ -253,150 +195,6 @@ set self4_p2p_slc1_avalon {
     172.16.112.1/32, # asgard1
     172.16.112.3/32, # asgard2
   }
-}
-
-
-# os-s-warp
-set self6_warp {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1101::1/128,
-    2a07:de40:b27e:1101::2/128,
-    2a07:de40:b27e:1101::3/128
-  }
-}
-
-# os-salt
-set self6_salt {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1200::1/128,
-    2a07:de40:b27e:1200::2/128,
-    2a07:de40:b27e:1200::3/128
-  }
-}
-
-# os-bare
-set self6_bare {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1201::1/128,
-    2a07:de40:b27e:1201::2/128,
-    2a07:de40:b27e:1201::3/128
-  }
-}
-
-# os-internal
-set self6_internal {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1203::1/128,
-    2a07:de40:b27e:1203::2/128,
-    2a07:de40:b27e:1203::3/128
-  }
-}
-
-# os-public
-set self6_public {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1204::1/128,
-    2a07:de40:b27e:1204::2/128,
-    2a07:de40:b27e:1204::3/128,
-  }
-}
-
-# os-mirror
-set self6_mirror {
-  type ipv6_addr
-  flags interval
-  elements = {
-    2a07:de40:b27e:1205::1/128,
-    2a07:de40:b27e:1205::2/128,
-    2a07:de40:b27e:1205::3/128
-  }
-}
-
-# os-odin
-set self6_odin {
-    type ipv6_addr
-    flags interval
-    elements = {
-        2a07:de40:b27e:1102::1/128,
-        2a07:de40:b27e:1102::2/128,
-        2a07:de40:b27e:1102::3/128,
-    }
-}
-
-# os-ghr-c
-set self6_ghr-c {
-    type ipv6_addr
-    flags interval
-    elements = {
-        2a07:de40:b27e:1207::1/128,
-        2a07:de40:b27e:1207::2/128,
-        2a07:de40:b27e:1207::3/128,
-    }
-}
-
-# os-ghr-os
-set self6_ghr-os {
-    type ipv6_addr
-    flags interval
-    elements = {
-        2a07:de40:b27e:1208::1/128,
-        2a07:de40:b27e:1208::2/128,
-        2a07:de40:b27e:1208::3/128,
-    }
-}
-
-# os-mail
-set self6_mail {
-    type ipv6_addr
-    flags interval
-    elements = {
-        2a07:de40:b27e:1209::1/128,
-        2a07:de40:b27e:1209::2/128,
-        2a07:de40:b27e:1209::3/128,
-    }
-}
-
-# os-kani
-set self6_kani {
-    type ipv6_addr
-    flags interval
-    elements = {
-        2a07:de40:b27e:1210::1/128,
-        2a07:de40:b27e:1210::2/128,
-        2a07:de40:b27e:1210::3/128,
-    }
-}
-
-# os-netbox
-set self6_netbox {
-    type ipv6_addr
-    flags interval
-    elements = {
-        2a07:de40:b27e:1211::1/128,
-        2a07:de40:b27e:1211::2/128,
-        2a07:de40:b27e:1211::3/128,
-    }
-}
-
-# os-log
-set self6_log {
-    type ipv6_addr
-    flags interval
-    elements = {
-        2a07:de40:b27e:1215::1/128,
-        2a07:de40:b27e:1215::2/128,
-        2a07:de40:b27e:1215::3/128,
-    }
 }
 
 ## Other individual hosts

salt/files/nftables/asgard/zones/1100_os-thor.nft
@@ -7,7 +7,7 @@
   ip6 nexthdr icmpv6 accept
 
   # thor within PRG2
-  ip6 saddr @host6_thor ip6 daddr @lan_ipv6 tcp dport 22 log prefix "[asgard] Thor SSH Allowed: " accept
+  ip6 saddr @host6_thor ip6 daddr @lan_ipv6_prg2 tcp dport 22 log prefix "[asgard] Thor SSH Allowed: " accept
   # thor to SLC1
   ip6 saddr @host6_thor ip6 daddr @host6_p2p_avalon tcp dport ssh accept
   ip6 saddr @host6_thor ip6 daddr @host6_avalon tcp dport ssh accept

salt/files/nftables/asgard/zones/1101_os-s-warp.nft
@@ -4,9 +4,9 @@
 
  chain input_network_os-s-warp {
   jump global_internal
-  ip6 saddr @net6_os-s-warp ip6 daddr @lan_ipv6 accept
+  ip6 saddr @net6_os-s-warp ip6 daddr @lan_ipv6_prg2 accept
   ip6 nexthdr icmpv6 accept
-  ip6 saddr @host6_warp ip6 daddr @self6_warp tcp dport 22 log prefix "[asgard] Inbound Allowed: " accept
+  ip6 saddr @host6_warp ip6 daddr @self6_s-warp tcp dport 22 log prefix "[asgard] Inbound Allowed: " accept
 
   log prefix "[Warp Denied] " reject with icmpx type admin-prohibited
 

salt/files/nftables/asgard/zones/1200_os-salt.nft
@@ -5,8 +5,8 @@
  chain input_network_os-salt {
   jump global_internal
 
-  ip6 saddr @lan_ipv6 ip6 daddr $net6_os-salt ip6 nexthdr icmpv6 accept
-  ip6 saddr $net6_os-salt ip6 daddr @lan_ipv6 ip6 nexthdr icmpv6 accept
+  ip6 saddr @lan_ipv6_prg2 ip6 daddr $net6_os-salt ip6 nexthdr icmpv6 accept
+  ip6 saddr $net6_os-salt ip6 daddr @lan_ipv6_prg2 ip6 nexthdr icmpv6 accept
 
   # os-salt -> mickey (for GitLab repository access via HTTPS)
   ip6 saddr $net6_os-salt ip6 daddr $host6_mickey tcp dport https accept

salt/files/nftables/asgard/zones/1202_os-ipmi-r.nft
@@ -6,3 +6,6 @@
   jump global_internal
 
  }
+
+ chain output_network_os-ipmi-r {
+ }

salt/files/nftables/asgard/zones/1203_os-internal.nft
@@ -74,7 +74,7 @@
   ip6 saddr @acl6_internet_openqa ip6 daddr @cloud6_openqa tcp dport https accept
 
   # jekyll -> * (HTTP/HTTPS, for planet.o.o RSS feeds)
-  oifname { nat64, os-p2p-pub } ip6 saddr $host6_jekyll tcp dport { http, https } ip6 daddr != @lan_ipv6_nonat accept
+  oifname { nat64, os-p2p-pub } ip6 saddr $host6_jekyll tcp dport { http, https } ip6 daddr != @lan_ipv6_prg2_nonat accept
 
   # jekyll -> atlas (HTTPS for planet.o.o to news.o.o)
   oif os-public ip6 saddr $host6_jekyll ip6 daddr $host6_atlas tcp dport https accept
@@ -99,20 +99,20 @@
   oifname @if_s2s ip6 saddr @host6_gitlab-runner ip6 daddr @host6_volva tcp dport rsync accept
 
   # matrix -> * (Matrix federation, all ports to facilitate connectivity to other homeservers using arbitrary ports)
-  oifname { nat64, os-p2p-pub } ip6 saddr $host6_matrix ip6 nexthdr tcp ip6 daddr != @lan_ipv6_nonat accept
+  oifname { nat64, os-p2p-pub } ip6 saddr $host6_matrix ip6 nexthdr tcp ip6 daddr != @lan_ipv6_prg2_nonat accept
   # matrix -> atlas (HTTPS for access to "itself", for example Telegram tries to chat with Matrix over its public matrix.o.o proxy address)
   oif os-public ip6 saddr $host6_matrix ip6 daddr $host6_atlas tcp dport https accept
 
   # discourse -> * (would be good to restrict further, but download of assets during discourse-update as well as generating of arbitrary link previews makes it difficult)
-  oifname { nat64, os-p2p-pub } ip6 saddr $host6_discourse tcp dport { http, https } ip6 daddr != @lan_ipv6_nonat log prefix "[Discourse HTTP 1] " accept
+  oifname { nat64, os-p2p-pub } ip6 saddr $host6_discourse tcp dport { http, https } ip6 daddr != @lan_ipv6_prg2_nonat log prefix "[Discourse HTTP 1] " accept
   # osc-collab -> * (https://github.com/openSUSE/osc-plugin-collab/blob/master/server/upstream/upstream-tarballs.txt)
-  oifname { nat64, os-p2p-pub } ip6 saddr $host6_osc-collab tcp dport { http, https, ftp } ip6 daddr != @lan_ipv6_nonat accept
+  oifname { nat64, os-p2p-pub } ip6 saddr $host6_osc-collab tcp dport { http, https, ftp } ip6 daddr != @lan_ipv6_prg2_nonat accept
 
   # monitor -> LiberaChat (IRCS for heroes-bot)
   oif os-p2p-pub ip6 saddr $host6_monitor ip6 daddr @cloud6_liberachat tcp dport ircs-u accept
 
   # monitor -> * (Prometheus)
-  ip6 saddr $host6_monitor ip6 daddr @lan_ipv6 tcp dport 9100 accept # Node Exporter
+  ip6 saddr $host6_monitor ip6 daddr @lan_ipv6_prg2 tcp dport 9100 accept # Node Exporter
   ip6 saddr $host6_monitor ip6 daddr @net6_os-s-warp tcp dport 9100 accept
   ip6 saddr $host6_monitor ip6 daddr @lan_ipv6_slc1 tcp dport 9100 accept
   ip6 saddr $host6_monitor ip6 daddr @host6_avalon tcp dport 9100 accept

commit 0ac98f1c5059614a7110670dd85d3276b77c5124

Load nftables networks from pillar
States cannot access files in the pillar root, load network data into
the pillar and implement logic to simulate the needed parts of the
pillar in the nftables test to avoid the need for loading a minion.
This was missed in the original templating implementation and only found
upon deployment through the production master.

Fixes: ab109e187832 ("Asgard: first nftables refactoring")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/role/gateway.sls
@@ -3,6 +3,10 @@ nftables: true
 {%- import_yaml 'infra/networks.yaml' as networks %}
 {%- set pseudo_networks = networks.pop('pseudo') %}
 
+networks:
+  sites: {{ networks }}
+  pseudos: {{ pseudo_networks }}
+
 {%- set export_networks = {
           'prg2': {'v4': [], 'v6': []},
           'slc1': {'v4': [], 'v6': []},

salt/files/nftables/asgard/base_inet/forward.nft
@@ -2,8 +2,7 @@
 ## MANAGED BY SALT in salt/files/nftables/asgard ##
 ###################################################
 
-{%- import_yaml 'pillar/infra/networks.yaml' as networks %}
-{%- set pseudo_networks = networks.pop('pseudo') %}
+{%- set site_networks = pillar['networks']['sites'] %}
 {%- set chainmap = {
           'iif': 'input',
           'oif': 'output',
@@ -20,7 +19,7 @@
     {%- if x == 'iif' %}
     os-asgard                      : jump network_os-asgard,
     {%- endif %}
-    {%- for network, network_config in networks['prg2'].items() | sort(attribute='1.short') %}
+    {%- for network, network_config in site_networks['prg2'].items() | sort(attribute='1.short') %}
       {%- set shortnet = network_config['short'] %}
     {{ shortnet }} {{ ( ': jump ' ~ chainmap[x] ~ '_network_' ~ shortnet ) | indent(30 - shortnet | length, true) }},
     {%- endfor %}

salt/files/nftables/asgard/base_inet/input.nft
@@ -2,8 +2,7 @@
 ## MANAGED BY SALT in salt/files/nftables/asgard ##
 ###################################################
 
-{%- import_yaml 'pillar/infra/networks.yaml' as networks %}
-{%- set pseudo_networks = networks.pop('pseudo') %}
+{%- set site_networks = pillar['networks']['sites'] %}
 
  # input chain, this is for traffic targeted towards the local system (i.e. this firewall itself)
  chain input {
@@ -15,7 +14,7 @@
 
   iif vmap {
     os-asgard                      : jump network_os-asgard,
-    {%- for network, network_config in networks['prg2'].items() | sort(attribute='1.short') %}
+    {%- for network, network_config in site_networks['prg2'].items() | sort(attribute='1.short') %}
       {%- set shortnet = network_config['short'] %}
     {{ shortnet }} {{ ( ': jump input_network_' ~ shortnet ) | indent(30 - shortnet | length, true) }},
     {%- endfor %}

salt/files/nftables/asgard/base_inet/sets.nft
@@ -7,10 +7,9 @@
 # 2. Addresses of this firewall cluster
 # 3. Addresses of other hosts
 
-{%- import_yaml 'pillar/infra/networks.yaml' as networks %}
-{%- set pseudo_networks = networks.pop('pseudo') %}
+{%- set networks = pillar['networks'] %}
 
-{% for site, site_networks in networks.items() %}
+{% for site, site_networks in networks['sites'].items() %}
 ###
 #  BEGIN: {{ site | upper }}
 ###
@@ -28,7 +27,7 @@ set lan_ipv6_{{ site }}{{ '' if with_nat else '_nonat' }} {
     $host6_slimhat,
     {%- endif %}
     {%- if with_nat %}
-    {{ pseudo_networks[site]['openSUSE-NAT64-Pool']['net6'] }},
+    {{ networks['pseudos'][site]['openSUSE-NAT64-Pool']['net6'] }},
     {%- endif %}
   }
 }

bin/test_nftables.pl
@@ -253,7 +253,19 @@ def get_grains(path='/etc/salt/grains'):
   except FileNotFoundError:
     return {}
 
+# networks pillar access without real minion
+def get_networks(path='pillar/infra/networks.yaml'):
+  networks = {}
+  with open(path) as file:
+    data = safe_load(file)
+
+  networks['pseudos'] = data.pop('pseudo')
+  networks['sites'] = data
+
+  return networks
+
 grains = get_grains()
+networks = get_networks()
 
 def render_file(path):
   if path is None:
@@ -265,6 +277,9 @@ def render_file(path):
       'grains': grains,
       'opts': {
         'cachedir': '/dev/null'
+      },
+      'pillar': {
+        'networks': networks,
       },
       'saltenv': None
     }, '.'