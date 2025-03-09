openSUSE/heroes-salt

commit 6c0c3eca185f0af8f15e0fe63f022bdcd3c57f23

Simplify custom grains tests
The differentiation between "global" and "localized" grains has already
been dropped a long time ago. Remove these leftovers and merge the
"site" grain test into the same logic used with other grains. Have the
printing functionality simply return all valid custom grains, as there
is no known use for the script to print only the sites (if such is
needed, it can easily be added at any time).

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

bin/get_valid_custom_grains.py
@@ -14,23 +14,8 @@ def get_valid_custom_grains():
     return VALID_CUSTOM_GRAINS
 
 
-def get_valid_global_grains():
-    return get_valid_custom_grains()['global']
-
-
-def get_sites():
-    return get_valid_custom_grains()['sites']
-
-
-def print_valid_localized_grains():
-    results = []
-    for site in get_sites():
-        results.append(site)
-    print('\n'.join(results))
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description='Loads the pillar/valid_custom_grains.py and returns a list of valid custom grains in the form of "site".')
     args = parser.parse_args()
 
-    print_valid_localized_grains()
+    print(get_valid_custom_grains())

bin/test_custom_grains.py
@@ -7,7 +7,7 @@ import sys
 
 import yaml
 from get_roles import read_file_skip_jinja
-from get_valid_custom_grains import get_sites, get_valid_global_grains
+from get_valid_custom_grains import get_valid_custom_grains
 
 
 def error_msg(sls, key, valid_values):
@@ -39,9 +39,6 @@ def test_custom_grain(mygrains, sls, key, valid_values, status):
 
 status = 0
 
-valid_global_grains = get_valid_global_grains()
-all_sites = get_sites()
-
 all_ids = sorted(os.listdir('pillar/id'))
 for sls in all_ids:
     if sls == 'README.md':
@@ -50,10 +47,7 @@ for sls in all_ids:
     content = read_file_skip_jinja(f'pillar/id/{sls}')
     mygrains = yaml.safe_load(content)['grains']
 
-    for key, valid_values in valid_global_grains.items():
+    for key, valid_values in get_valid_custom_grains().items():
         status = test_custom_grain(mygrains, sls, key, valid_values, status)
-
-    if 'site' not in mygrains or mygrains['site'] not in all_sites:
-        status = error_msg(sls, 'site', all_sites)
 
 sys.exit(status)

pillar/valid_custom_grains.yaml
@@ -1,13 +1,12 @@
 ---
-global:
-  hostusage: []
-  reboot_safe:
-    # yamllint disable rule:truthy
-    - yes
-    - no
-    # yamllint enable rule:truthy
-    - unknown
-sites:
+hostusage: []
+reboot_safe:
+  # yamllint disable rule:truthy
+  - yes
+  - no
+  # yamllint enable rule:truthy
+  - unknown
+site:
   - nue-ipx
   - prg2
   - slc1

commit 060eda2318aacf7c5944748fd4f08689caf05f7b

Print valid sites from custom grains again
This was accidentally removed albeit test_show_highstate depending on
it, causing the test to iterate over bogus data. Introduce it again,
but this time behind an argument making the behavior more apparent.

Fixes: 6c0c3eca185f ("Simplify custom grains tests")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

bin/get_valid_custom_grains.py
@@ -13,9 +13,15 @@ def get_valid_custom_grains():
 
     return VALID_CUSTOM_GRAINS
 
+def get_valid_sites():
+    return get_valid_custom_grains()['site']
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description='Loads the pillar/valid_custom_grains.py and returns a list of valid custom grains in the form of "site".')
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description='Loads and prints pillar/valid_custom_grains.yaml.')
+    parser.add_argument('-s', '--sites', action='store_true', help='Only print valid sites.')
     args = parser.parse_args()
 
-    print(get_valid_custom_grains())
+    if args.sites:
+      print('\n'.join(get_valid_sites()))
+    else:
+      print(get_valid_custom_grains())

bin/test_show_highstate.sh
@@ -46,7 +46,7 @@ show_highstate() {
     echo
 }
 
-ALL_LOCATIONS=( $(bin/get_valid_custom_grains.py) )
+ALL_LOCATIONS=( $(bin/get_valid_custom_grains.py -s) )
 for site in "${ALL_LOCATIONS[@]}"; do
     show_highstate "$site" 'infra.opensuse.org' 'kvm'
 done