openSUSE/heroes-salt

commit 55620b9b4a41ed12125b5292086669a7a9edd704

Atlas: overwrite src for http-login
To allow future SPOE configuration referencing "src" to work without the
need for special handling of the header serving the real source IP address
from the login proxy, set "src" to always serve the source IP address from the
relevant header. As the login proxies are always the same, there is no use for
keeping their source address anyways.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/atlas/init.sls
@@ -95,7 +95,8 @@ haproxy:
           - accept unless !cookie_os_session path_indexphp param_mw_days_from param_mw_hide param_mw_high_limit
           - accept if WAIT_END
       httprequests:
-        - track-sc0: req.hdr_ip(X-Forwarded-For,-1)
+        - set-src: req.hdr_ip(X-Forwarded-For,-1)
+        - track-sc0: src
         - deny:
           - deny_status 403 if annoying_useragents
           - deny_status 429 if annoying_networks

commit 24f135575d37f3a69d4772fb1932342a85ea8887

Atlas: exclude login proxies from src override
The use_backend rules are conditionalized on the login proxy source
addresses, hence those need to be evaluated before src is overwritten.

Fixes: 55620b9b4a41 ("Atlas: overwrite src for http-login")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/atlas/init.sls
@@ -96,6 +96,7 @@ haproxy:
           - accept unless !cookie_os_session path_indexphp param_mw_days_from param_mw_hide param_mw_high_limit
           - accept if WAIT_END
       httprequests:
+        - set-var(req.is_src_login): bool(true) if src_login_pre
         - set-src: req.hdr_ip(X-Forwarded-For,-1)
         - track-sc0: src
         - deny:

pillar/cluster/atlas/services.sls
@@ -280,8 +280,9 @@ haproxy:
 
     # services routed from login proxies
     http-login:
-      acls:                              # daffy1              # daffy2
-        - src_login         src          2a07:de40:b280:86::11 2a07:de40:b280:86::12
+      acls:                                # daffy1              # daffy2
+        - src_login_pre       src          2a07:de40:b280:86::11 2a07:de40:b280:86::12
+        - src_login           var(req.is_src_login) -m bool
         - annoying_networks   req.hdr_ip(X-Forwarded-For)  -f /etc/haproxy/blacklists/networks -n
         - annoying_useragents hdr_sub(User-Agent)          -i -f /etc/haproxy/blacklists/useragents
         - odd_clients         req.hdr_cnt(Accept-Language) 0