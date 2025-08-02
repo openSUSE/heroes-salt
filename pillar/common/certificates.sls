openSUSE/heroes-salt

commit 3acb4821d778e3238d3916caf6f40d80f7b59008

Add space after sftp rule
Upon scp the sftp command in SSH_ORIGINAL_COMMAND is suffixed with a
whitespace, which needs to be reflected in order for the expression to
match.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/common/certificates.sls
@@ -83,7 +83,7 @@ def run():
           'certificate_deployment': {
             'cert': {
               'commands': [
-                sftp,
+                f'{sftp} ',
                 r'systemctl is-active --quiet mariadb \|\| exit "$\?" ; mariadb-admin -S /run/mysql/mysql.sock --connect-timeout=10 --wait=2 flush-ssl',  # noqa W605
               ],
             },

commit 0eda7af7b2909f386a28f5c8de0baf8da87b450c

Optional space after sftp rule
It seems the trailing space is only present on Leap based target hosts.
For simplicity, make it generally optional.

Fixes: 3acb4821d778 ("Add space after sftp rule")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/common/certificates.sls
@@ -83,7 +83,7 @@ def run():
           'certificate_deployment': {
             'cert': {
               'commands': [
-                f'{sftp} ',
+                f'{sftp} ?',
                 r'systemctl is-active --quiet mariadb \|\| exit "\$\?" ; mariadb-admin -S /run/mysql/mysql.sock --connect-timeout=10 --wait=2 flush-ssl',  # noqa W605
               ],
             },