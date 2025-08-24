openSUSE/heroes-salt

commit 6cf4862ef9d8d0c4087931059a664cb363222e35

Modernize Falkor/Squanchy network pillars
Align with the newer layout from Arches/Chaplin and use macros avoiding
the need for duplication of VLAN IDs and adding management of default
routes.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/squanchy/init.sls
@@ -1,4 +1,4 @@
-{%- from 'macros.jinja' import bond, slave, smart, vlantap %}
+{%- from 'macros.jinja' import bond, default_gateway, slave, vlantap, vlantapnetworks %}
 
 network:
   interfaces:
@@ -22,25 +22,25 @@ network:
     {{ vlantap('os-ipmi-ur', 1001, 'bond-mgmt') }}
 
     # VLAN interfaces for generic VM connectivity
-    {%- set vlanmap = {
-          'os-thor': 1100,
-          'os-internal': 1203,
-          'os-mirror': 1205,
-          'os-salt': 1200,
-          'os-s-warp': 1101,
-          's-j-os-out': 3202,
-          's-na-mgmt': 3339
-        }
+    {%- set vlanmap_r = [
+          'os-internal',
+          'os-mirror',
+          'os-s-warp',
+          'os-salt',
+          'os-thor',
+          's-j-os-out',
+          's-na-mgmt',
+        ]
     %}
-    {%- for vlan_name, vlan_id in vlanmap.items() %}
-    {{ vlantap(vlan_name, vlan_id, 'bond-ob') }}
-    {%- endfor %}
+    {{ vlantapnetworks(vlanlist_r, 'bond-ob', 'prg2') }}
+
+  {{ default_gateway('prg2', 'openSUSE-bare') }}
 
 firewalld:
   zones:
     drop:
       interfaces:
-        {%- for vlan_name in vlanmap.keys() %}
+        {%- for vlan_name in vlanlist_r %}
         - x-{{ vlan_name }}
         {%- endfor %}
 

pillar/cluster/falkor/network.sls
@@ -1,4 +1,4 @@
-{%- from 'macros.jinja' import bond, slave, vlantap %}
+{%- from 'macros.jinja' import bond, default_gateway, slave, vlantapnetworks %}
 
 network:
   interfaces:
@@ -27,32 +27,32 @@ network:
       firewall: false
 
     # VLAN interfaces for generic (non-VRRP) VM connectivity
-    {%- set vlanmap = {
-          'os-thor': 1100,
-          'os-odin': 1102,
-          'os-salt': 1200,
-          'os-internal': 1203,
-          'os-public': 1204,
-          'os-mirror': 1205,
-          'os-code': 1206,
-          'os-mail': 1209,
-          'os-kani': 1210,
-          'os-netbox': 1211,
-          'os-log': 1215,
-          'os-kani-dev': 1216,
-          'os-kani-ext': 1217,
-          'os-web': 1218,
-          'os-code-dev': 1219,
-        }
+    {%- set vlanlist_r = [
+          'os-code',
+          'os-code-dev',
+          'os-internal',
+          'os-kani',
+          'os-kani-dev',
+          'os-kani-ext',
+          'os-log',
+          'os-mail',
+          'os-mirror',
+          'os-netbox',
+          'os-odin',
+          'os-public',
+          'os-salt',
+          'os-thor',
+          'os-web',
+        ]
     %}
-    {%- for vlan_name, vlan_id in vlanmap.items() %}
-    {{ vlantap(vlan_name, vlan_id, 'bond-fib') }}
-    {%- endfor %}
+    {{ vlantapnetworks(vlanlist_r, 'bond-fib', 'prg2') }}
+
+  {{ default_gateway('prg2', 'openSUSE-bare') }}
 
 firewalld:
   zones:
     drop:
       interfaces:
-        {%- for vlan_name in vlanmap.keys() %}
+        {%- for vlan_name in vlanlist_r %}
         - x-{{ vlan_name }}
         {%- endfor %}

commit aff0b484bed30384b63084330deb22fa3a70d78e

Correct Squanchy vlanlist variable name
Fixes: 6cf4862ef9d8 ("Modernize Falkor/Squanchy network pillars")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/squanchy/init.sls
@@ -22,7 +22,7 @@ network:
     {{ vlantap('os-ipmi-ur', 1001, 'bond-mgmt') }}
 
     # VLAN interfaces for generic VM connectivity
-    {%- set vlanmap_r = [
+    {%- set vlanlist_r = [
           'os-internal',
           'os-mirror',
           'os-s-warp',