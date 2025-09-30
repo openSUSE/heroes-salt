openSUSE/heroes-salt

commit 3085098a73cdc0fc0644ba79c1e3a22126abb7fb

Change sync image
- Use container image with old rsync 3.2.3 to work around defective
  --chown in rsync 3.2.7:
  https://bugzilla.opensuse.org/show_bug.cgi?id=1226656
- Simplify call by removing options already implied through -a

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

.gitlab-ci.yml
@@ -119,8 +119,9 @@ test_nginx:
 
 sync:
   extends: .deploy
+  image: registry.opensuse.org/opensuse/infrastructure/containers/heroes-salt-deployment-rsync:latest
   script:
-    - "rsync -a --delete --super --owner --group --chown=477:479 --chmod='u=rwX,g=rX,o=' $PWD/ rsync://saltpush@witch1.infra.opensuse.org:873/salt-push/"
+    - "rsync -a --delete --super --chown=477:479 --chmod='u=rwX,g=rX,o=' $PWD/ rsync://saltpush@witch1.infra.opensuse.org:873/salt-push/"
 
 deploy:
   extends: .deploy

commit 5d2ff74af6f37db8761f6133d438be9a5adde4e0

Change back sync image
Following the advice in
https://bugzilla.opensuse.org/show_bug.cgi?id=1226656#c10, switch back
to default rsync version with the option needed to make our other
arguments continue to behave as intended.

Fixes: 3085098a73cd ("Change sync image")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

.gitlab-ci.yml
@@ -143,7 +143,6 @@ test_nginx:
 
 sync:
   extends: .deploy
-  image: registry.opensuse.org/opensuse/infrastructure/containers/heroes-salt-deployment-rsync:latest
   script: bin/sync.sh
   resource_group: production
 

bin/sync.sh
@@ -10,7 +10,7 @@ for master in \
 	witch1 volva1 volva2
 do
 	printf 'Transferring to %s ... ' "$master"
-	if rsync -a --delete --super --chown=cloneboy:salt --chmod='u=rwX,g=rX,o=' --contimeout=20 --timeout=90 . saltpush@"$master".infra.opensuse.org::salt-push
+	if rsync -a --delete --protect-args --super --chown=cloneboy:salt --chmod='u=rwX,g=rX,o=' --contimeout=20 --timeout=90 . saltpush@"$master".infra.opensuse.org::salt-push
 	then
 		printf '\e[32m%s\e[0m\n' 'OK'
 	else