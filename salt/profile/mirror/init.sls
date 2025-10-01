openSUSE/heroes-salt

commit 17c2e038cc5057bd141ee58324743a9be2e01cb3

Add distribution loop script
Needed to sync GA repositories (distribution/).

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/profile/mirror/init.sls
@@ -44,6 +44,7 @@ mirror_cscreenrc:
         - zombie "x "
         - log on
         - zombie_timeout 11
+        - screen -L 0:sync_distribution   bash --login -c "/home/mirror/bin/loop_distribution.sh"
         - screen -L 0:sync_updates        bash --login -c "/home/mirror/bin/loop_update.sh"
         - screen -L 0:sync_tumbleweed     bash --login -c "/home/mirror/bin/loop_tumbleweed.sh"
         - screen -L 0:sync_repositories   bash --login -c "/home/mirror/bin/loop_repositories.sh"

salt/profile/mirror/files/bin/loop_distribution.sh.jinja
@@ -1,0 +1,33 @@
+#!/bin/bash
+{{ pillar['managed_by_salt'] }}
+
+WHAT=( 'distribution/leap/15.5' 'distribution/leap/15.6' )
+
+. /home/mirror/bin/loop_common
+
+RSYNC_OPTIONS="$RSYNC_OPTIONS 
+        --exclude=appliances/
+        --exclude=ChangeLogs/
+	--exclude=iso/
+	--exclude=live/
+	--exclude=repo/non-oss/
+        --exclude=*aarch64*
+	--exclude=*aws*
+	--exclude=*azure*
+	--exclude=*bluetooth*
+        --exclude=*debuginfo*
+	--exclude=*libreoffice*
+	--exclude=*lxqt*
+	--exclude=*texlive*
+        --exclude=*virtualbox*
+	--exclude=*ppc64le*
+	--exclude=*s390x*
+        --exclude=*Mesa*
+        --exclude=*Mozilla*
+        --exclude=*chromium*
+	--exclude=*-cross*
+        --exclude=cross-*
+        --exclude=NetworkManager*
+"
+
+sync_loop

commit bb3ab24d6349e499a46024991ccdce57e8f3b0ba

Manage distribution loop script
The file was tracked in the repository but forgotten to be listed in
the files to manage in the relevant Salt state.

Fixes: 17c2e038cc50 ("Add distribution loop script")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/profile/mirror/init.sls
@@ -7,6 +7,7 @@ mirror_bin:
     - names:
         {%- for file in [
               'loop_common',
+              'loop_distribution.sh',
               'loop_repositories.sh',
               'loop_tumbleweed.sh',
               'loop_update.sh',