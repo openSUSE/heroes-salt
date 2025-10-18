openSUSE/heroes-salt

commit 98e05514613cea5307db702cd158f1cde53bdcc3

Copy nsswitch.conf before replacement
The default file is under /usr/etc/ now, the custom file needs to be
present before file.replace, otherwise the state will fail.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/profile/kanidm/client/init.sls
@@ -2,12 +2,21 @@ include:
   - zypper.packages
   - .legacy
 
+/etc/nsswitch.conf_copy:
+  file.copy:
+    - name: /etc/nsswitch.conf
+    - source: /usr/etc/nsswitch.conf
+    - preserve: True
+
 {%- for setting in ['passwd', 'group'] %}
 /etc/nsswitch.conf_{{ setting }}:
   file.replace:
     - name: /etc/nsswitch.conf
+    - ignore_if_missing: {{ opts['test'] }}
     - pattern: ^{{ setting }}:.*$
     - repl: '{{ setting }}: compat kanidm'
+    - require:
+        - file: /etc/nsswitch.conf_copy
 {%- endfor %}
 
 kanidm_config:

commit a3095eef2899e138f77937f8efa150ebc1a65a17

Guard nsswitch.conf copy
The default file under /usr/etc/ is only present after recent Tumbleweed
and Leap 16.

Fixes: 98e05514613c ("Copy nsswitch.conf before replacement")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/profile/kanidm/client/init.sls
@@ -2,11 +2,16 @@ include:
   - zypper.packages
   - .legacy
 
+{%- if grains['osrelease'] | float >= 16 %}
 /etc/nsswitch.conf_copy:
   file.copy:
     - name: /etc/nsswitch.conf
     - source: /usr/etc/nsswitch.conf
     - preserve: True
+    - require_in:
+        - file: /etc/nsswitch.conf_passwd
+        - file: /etc/nsswitch.conf_group
+{%- endif %}
 
 {%- for setting in ['passwd', 'group'] %}
 /etc/nsswitch.conf_{{ setting }}:
@@ -15,8 +20,6 @@ include:
     - ignore_if_missing: {{ opts['test'] }}
     - pattern: ^{{ setting }}:.*$
     - repl: '{{ setting }}: compat kanidm'
-    - require:
-        - file: /etc/nsswitch.conf_copy
 {%- endfor %}
 
 kanidm_config: