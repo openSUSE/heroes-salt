openSUSE/heroes-salt

commit ee4c8024145ab13176261889a7e37123e69214c7

Manage default gateways in all cluster pillars
Only some had it so far; unify to ensure routes are managed and enforced
on all machines.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/apollo/cobbler/network.sls
@@ -19,3 +19,5 @@ network:
       etherdevice: bond-ob
       vlan_id: 1207
       firewall: false
+
+  {{ default_gateway('prg2', 'openSUSE-GHR-Cobbler') }}

pillar/cluster/chaplin/network.sls
@@ -34,6 +34,8 @@ network:
     # VLAN interface for external warp VM connectivity
     {{ vlantap('s-j-os-out', 1011, 'bond-ob') }}
 
+  {{ default_gateway('slc1', 'openSUSE-bare') }}
+
 firewalld:
   enabled: true
   zones:

pillar/cluster/kepler/network.sls
@@ -22,6 +22,8 @@ network:
     # VLAN interfaces for generic VM connectivity
     {{ vlantap('os-avalon', 1700, 'bond-ob') }}
 
+  {{ default_gateway('slc1', 'openSUSE-bare') }}
+
 firewalld:
   enabled: true
   zones:

pillar/cluster/orbit/init.sls
@@ -31,6 +31,8 @@ network:
     # VLAN interfaces for generic (non-VRRP) VM connectivity
     {{ vlantap('os-internal', 1203, 'bond-ob') }}
 
+  {{ default_gateway('prg2', 'openSUSE-bare') }}
+
 firewalld:
   enabled: true
   zones:

commit 063d446755f90fe44b5685704c37f1454c931760

Add missing dfault_gateway macro imports
Fixes: ee4c8024145a ("Manage default gateways in all cluster pillars")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/apollo/cobbler/network.sls
@@ -1,4 +1,4 @@
-{%- from 'macros.jinja' import bond, slave %}
+{%- from 'macros.jinja' import bond, default_gateway, slave %}
 
 firewalld:
   zones:

pillar/cluster/chaplin/network.sls
@@ -1,4 +1,4 @@
-{%- from 'macros.jinja' import bond, slave, vlantap, vlantapnetworks %}
+{%- from 'macros.jinja' import bond, default_gateway, slave, vlantap, vlantapnetworks %}
 
 network:
   interfaces:

pillar/cluster/kepler/network.sls
@@ -1,4 +1,4 @@
-{%- from 'macros.jinja' import bond, slave, vlantap %}
+{%- from 'macros.jinja' import bond, default_gateway, slave, vlantap %}
 
 network:
   interfaces:

pillar/cluster/orbit/init.sls
@@ -1,4 +1,4 @@
-{%- from 'macros.jinja' import bond, slave, smart, vlantap %}
+{%- from 'macros.jinja' import bond, default_gateway, slave, smart, vlantap %}
 
 network:
   interfaces: