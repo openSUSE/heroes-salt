openSUSE/heroes-salt

commit 93b1c50a07a6ab75a86847d19b4f334cdc6ddf61

Initialize network and VMs for gitlab-runner{3,4}
The two existing runners are often reaching their capacity.
Add some more to allow for more parallel CI jobs, and place them in SLC
as we have much more spare room there. It is assumed that the additional
compute capacity for job load outweighs the added network latency - we
will need to test if that holds true.
Whilst at it, improve the host metadata and set myself as maintainer.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/arches/suse_ha.sls
@@ -3,6 +3,7 @@ suse_ha:
     name: arches
   constraints:
     {%- for vmpair in [
+          'gitlab-runner',
           'globus',
           'slc-monitor',
           'slc-ns',

pillar/cluster/arches/network.sls
@@ -32,6 +32,7 @@ network:
     # VLAN interfaces for generic VM connectivity
     {%- set vlanlist_r = [
           'os-dbginfod',
+          'os-glr',
           'os-internal',
           'os-mircache',
           'os-mirror',

pillar/id/gitlab-runner1_infra_opensuse_org.sls
@@ -1,17 +1,19 @@
 grains:
   site: prg2
   hostusage:
-    - gitlab runner
+    - GitLab Runner
   reboot_safe: yes
 
   aliases: []
-  description: GitLab Runner
+  description: GitLab CI/CD runner/worker
   documentation:
     - https://docs.gitlab.com/runner/
   responsible:
-    - lrupp
+    - crameleon
   partners:
     - gitlab-runner2.infra.opensuse.org
+    - gitlab-runner3.infra.opensuse.org
+    - gitlab-runner4.infra.opensuse.org
   weburls: []
 roles:
   - gitlab_runner

pillar/id/gitlab-runner3_infra_opensuse_org.sls
@@ -1,5 +1,5 @@
 grains:
-  site: prg2
+  site: slc1
   hostusage:
     - GitLab Runner
   reboot_safe: yes
@@ -12,7 +12,7 @@ grains:
     - crameleon
   partners:
     - gitlab-runner1.infra.opensuse.org
-    - gitlab-runner3.infra.opensuse.org
+    - gitlab-runner2.infra.opensuse.org
     - gitlab-runner4.infra.opensuse.org
   weburls: []
 roles:

pillar/id/gitlab-runner4_infra_opensuse_org.sls
@@ -1,5 +1,5 @@
 grains:
-  site: prg2
+  site: slc1
   hostusage:
     - GitLab Runner
   reboot_safe: yes
@@ -12,8 +12,8 @@ grains:
     - crameleon
   partners:
     - gitlab-runner1.infra.opensuse.org
+    - gitlab-runner2.infra.opensuse.org
     - gitlab-runner3.infra.opensuse.org
-    - gitlab-runner4.infra.opensuse.org
   weburls: []
 roles:
   - gitlab_runner

pillar/infra/hosts.yaml
@@ -348,6 +348,30 @@ gitlab-runner2:
   legacy_boot: true
   ram: 24576MB
   vcpu: 32
+gitlab-runner3:
+  cluster: arches
+  disks:
+    data0: 3600a0980383143393724584a74517977
+    root: 3600a0980383143393724584a74517976
+  interfaces:
+    os-glr:
+      ip6: 2a07:de40:617e:1908::103/64
+      mac: 00:16:3e:7b:77:7e
+      source: x-os-glr
+  ram: 32768MB
+  vcpu: 36
+gitlab-runner4:
+  cluster: arches
+  disks:
+    data0: 3600a0980383143393724584a74517979
+    root: 3600a0980383143393724584a74517978
+  interfaces:
+    os-glr:
+      ip6: 2a07:de40:617e:1908::104/64
+      mac: 00:16:3e:7b:77:7f
+      source: x-os-glr
+  ram: 32768MB
+  vcpu: 36
 globus1:
   cluster: arches
   disks:

pillar/infra/networks.yaml
@@ -195,6 +195,12 @@ slc1:
     net6: 2a07:de40:617e:1907::/64
     gw6: 2a07:de40:617e:1907::3
     id: 1907
+  openSUSE-gitlab-runner:
+    export: true
+    short: os-glr
+    net6: 2a07:de40:617e:1908::/64
+    gw6: 2a07:de40:617e:1908::3
+    id: 1908
   openSUSE-monitor:
     export: true
     short: os-monitor

pillar/role/gitlab_runner.sls
@@ -1,3 +1,5 @@
+{%- set site = grains.get('site') %}
+
 include:
   - .docker
   # secrets include conditionalized inside the secrets files
@@ -16,8 +18,15 @@ profile:
       fixed-cidr-v6: 2a07:de40:b27e:400{{ grains['host'][-1] }}::/64
   gitlab_runner:
     config:
-      concurrent: 30
+    {%- if site == 'prg2' %}
       check_interval: 5
+      concurrent:  30
+    {%- elif site == 'slc1' %}
+      check_interval: 30
+      concurrent:  40
+    {%- else %}
+      {%- do salt.log.warning('gitlab_runner: possibly incomplete pillar') %}
+    {%- endif %}
       user: gitlab-runner
       shutdown_timeout: 0
       session_server:

commit 2f794799f37c08d1423bfd1fe658109d9e596287

Arches: correct gitlab-runner colocation
The vmpair iteration only works if the host indexes are 1 and 2.
If we have more with other indexes in the future we might want to move
this to a macro.

Fixes: 93b1c50a07a6 ("Initialize network and VMs for gitlab-runner{3,4}")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/arches/suse_ha.sls
@@ -3,7 +3,6 @@ suse_ha:
     name: arches
   constraints:
     {%- for vmpair in [
-          'gitlab-runner',
           'globus',
           'slc-monitor',
           'slc-ns',
@@ -17,6 +16,12 @@ suse_ha:
         - VM_{{ vmpair }}1.infra.opensuse.org
         - VM_{{ vmpair }}2.infra.opensuse.org
     {%- endfor %}
+    colo_gitlab-runner:
+      type: rsc_colocation
+      score: -100
+      resources:
+        - VM_gitlab-runner3.infra.opensuse.org
+        - VM_gitlab-runner4.infra.opensuse.org
   fencing:
     stonith_enable: true
     sbd: