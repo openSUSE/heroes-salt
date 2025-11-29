openSUSE/heroes-salt

commit dd424ab9f8d360c1b75c225174b42fb1945fa72a

Adjustments for HAProxy 3
Solve new warnings:

```
[WARNING]  (1468) : config : hlua: please set "tune.lua.bool-sample-conversion" tunable to either "normal" or "pre-3.1-bug" explicitly to avoid ambiguities. This must be set before any "lua-load" or "lua-load-per-thread" directive. Defaulting to "pre-3.1-bug".
[WARNING]  (1468) : Found 4 thread(s) mapped to a CPU and 28 thread(s) not mapped to any CPU. This will result in some threads being randomly assigned to the same CPU, which will occasionally cause severe performance degradation. First thread bound is 0 and first thread not bound is 4. Please either bind all threads or none (maybe some cpu-map directives are missing?).
```

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/common/public_proxy.sls
@@ -4,6 +4,7 @@
 haproxy:
   global:
     extra:
+      - tune.lua.bool-sample-conversion normal
       - lua-load-per-thread /etc/haproxy/geoip.lua
   frontends:
     http:

pillar/common/haproxy/global.sls
@@ -31,5 +31,7 @@ haproxy:
       - ssl-default-server-ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305  # noqa 204
       - ssl-default-server-ciphersuites TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256
       - ssl-default-server-options no-sslv3 no-tlsv10 no-tlsv11 no-tls-tickets
-      - nbthread {{ grains['num_cpus'] }}
-      - cpu-map auto:1/1-4 0-3
+      {%- set cpus = grains['num_cpus'] %}
+      - nbthread {{ cpus }}
+      # are these values good?
+      - cpu-map auto:1/1-{{ cpus }} 0-{{ cpus - 1 }}

commit 184d2bb01da75c698a00038d906300823bbc7a8b

Guard HAProxy 3 Lua sample conversion option
We still run HAProxy instances on Leap 15.6, where 2.x is shipped.
Resolve the "unknown keyword" error there.

Fixes: dd424ab9f8d3 ("Adjustments for HAProxy 3")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/common/public_proxy.sls
@@ -4,7 +4,9 @@
 haproxy:
   global:
     extra:
+      {%- if grains['osrelease'] | float >= 16 %}
       - tune.lua.bool-sample-conversion normal
+      {%- endif %}
       - lua-load-per-thread /etc/haproxy/geoip.lua
   frontends:
     http: