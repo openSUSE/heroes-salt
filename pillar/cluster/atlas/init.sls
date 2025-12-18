openSUSE/heroes-salt

commit 6b9ab0e1efedf4cb856e50709283cda17b716f2d

Change source address for factory-dashboard sync
Rsync connection now happens over IPv6.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/atlas/init.sls
@@ -142,7 +142,7 @@ haproxy:
     {{ metrics(bind_v6_standalone) }}
 
     rsync-community2:
-      acls: network_allowed src 195.135.223.25/32 # botmaster; additionaly restricted in border firewall
+      acls: network_allowed src 2a07:de40:b240:0:dc6f:dfb1:dc6f:dfb1/128 # botmaster; additionaly restricted in border firewall
       {{ rsync_backend_with_checks('2a07:de40:b27e:1203::129', extra='send-proxy', listen_addresses=bind_v4_vip, listen_port=11873, listen_params=bindopts) }}
 
     rsync-man:

pillar/role/factory-dashboard.sls
@@ -52,7 +52,7 @@ rsync:
       read only: false
       uid: dashboard
       hosts allow:
-        - 195.135.223.25/32
+        - 2a07:de40:b240:0:dc6f:dfb1:dc6f:dfb1/128
 
 users:
   dashboard: {}

salt/files/nftables/asgard/base_nat/prerouting.nft
@@ -7,8 +7,6 @@
    ip daddr $net4_internet2 ip protocol icmp dnat to $host4_atlas
    ip daddr $net4_internet2 tcp dport { http, https } dnat to $host4_atlas
 
-   # factory-dashboard rsync from botmaster
-   ip saddr 195.135.223.25/32 ip daddr $net4_internet2 tcp dport 11873 dnat to $host4_atlas
    # man (rpm2docserv output) rsync from pontifex (via obs-gateway)
    ip saddr @cloud4_obs-gateway ip daddr $net4_internet2 tcp dport 11874 dnat to $host4_atlas
 

salt/files/nftables/asgard/zones/3201_os-p2p-pub.nft
@@ -24,7 +24,7 @@
   ip daddr $host4_atlas_misc tcp dport { http, https } accept
 
   # rsync to atlas*.i.o.o (for factory-dashboard)
-  ip saddr 195.135.223.25/32 ip daddr @host4_atlas tcp dport 11873 log prefix "[Factory Dashboard Rsync] " accept
+  ip6 saddr 2a07:de40:b240:0:dc6f:dfb1:dc6f:dfb1/128 ip6 daddr $host6_atlas tcp dport 11873 log prefix "[Factory Dashboard Rsync] " accept
   # rsync to atlas*.i.o.o (for man)
   ip saddr @cloud4_obs-gateway ip daddr @host4_atlas tcp dport 11874 log prefix "[Man Rsync] " accept
 

commit 01542f68c5776a38ffedcd3d3a90d3a3fbb89dcc

Atlas: change community2 rsync listener to IPv6
As the connection happens over IPv6 now, the listener must bind to the
respective IPv6 address.

Fixes: 6b9ab0e1efed ("Change source address for factory-dashboard sync")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/atlas/init.sls
@@ -143,7 +143,7 @@ haproxy:
 
     rsync-community2:
       acls: network_allowed src 2a07:de40:b240:0:dc6f:dfb1:dc6f:dfb1/128 # botmaster; additionaly restricted in border firewall
-      {{ rsync_backend_with_checks('2a07:de40:b27e:1203::129', extra='send-proxy', listen_addresses=bind_v4_vip, listen_port=11873, listen_params=bindopts) }}
+      {{ rsync_backend_with_checks('2a07:de40:b27e:1203::129', extra='send-proxy', listen_addresses=bind_v6_vip, listen_port=11873, listen_params=bindopts) }}
 
     rsync-man:
       acls: network_allowed src 10.151.132.20/32 10.151.132.21/32 10.151.132.22/32  # obs-gateway; additionaly restricted in firewall