openSUSE/heroes-salt

commit 2202f14d66901101be9331a3cef5225d19af683d

Rename nftables service
The main nftables package now ships a nftables.service, and the service
in the custom nftables-service package was renamed. Adjust respectively,
we prefer to keep the custom nftables service as it supports
preservation of sets.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/profile/nftables/init.sls
@@ -26,7 +26,7 @@ nftables_config_tree:
 
 nftables_service:
   service.running:
-    - name: nftables
+    - name: nftables-full
     - enable: true
     - reload: true
     - require:

commit ecb13f47bb9acfe41025ba302b24bed0372751fb

Align nftables service name
Fixes: 2202f14d6690 ("Rename nftables service")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/profile/nftables/init.sls
@@ -37,7 +37,7 @@ nftables_service:
 
 nftables_status:
   cmd.run:
-    - name: systemctl --no-pager status -l nftables
+    - name: systemctl --no-pager status -l nftables-full
     - require:
         - pkg: nftables_packages
         - file: nftables_config_base