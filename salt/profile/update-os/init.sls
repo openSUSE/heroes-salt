openSUSE/heroes-salt

commit 157957bc3d6edb47da15bc95451b5a8ad186ca41

Add basic OS upgrade script
Populate machines with an outdated version of openSUSE Leap with a
script to make interactive upgrades more streamlined for
administrators.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/profile/update-os/init.sls
@@ -5,3 +5,21 @@
     - user: root
     - group: root
     - mode: '0744'
+
+{%- set latest = '16.0' %}
+{%- set previous = '15.6' %}
+
+/usr/local/sbin/upgrade-os:
+{%- if grains['osrelease'] == latest or grains['osfullname'] != 'Leap' %}
+  file.absent
+{%- else %}
+  file.managed:
+    - source: salt://{{ slspath }}/files/upgrade-os.sh
+    - template: jinja
+    - context:
+        latest_version: '{{ latest }}'
+        previous_version: '{{ previous }}'
+    - user: root
+    - group: root
+    - mode: '0755'
+{%- endif %}

salt/profile/update-os/files/update-os.sh.jinja
@@ -1,4 +1,6 @@
 #!/bin/sh -Cfu
+# Script for installing the latest updates. Note that updates are typically installed through the os-update service.
+# Author: Georg Pfuetzenreuter
 {#
 vim: ft=sh.jinja
 -#}{{ pillar['managed_by_salt'] }}

salt/profile/update-os/files/upgrade-os.sh.jinja
@@ -1,0 +1,60 @@
+#!/bin/sh -Cefu
+# Script for upgrading to a new Leap release.
+# Author: Georg Pfuetzenreuter
+#{{ pillar['managed_by_salt'] }}
+
+old_ver='{{ previous_version }}'
+new_ver='{{ latest_version }}'
+
+. /etc/os-release
+
+if [ "$VERSION" != "$old_ver" ]
+then
+  echo 'Upgrade from unsupported OS attempted, aborting.'
+  exit 1
+fi
+
+_zypper () {
+  zypper --releasever="$new_ver" "$@"
+}
+
+if _zypper -qt lr -u | grep -P ^\\d | grep -v "$new_ver"
+then
+  # shellcheck disable=SC2016
+  echo 'Repository URLs not respecting $releasever found, aborting.'
+  exit 1
+fi
+
+repositories="$(_zypper -qt lr -E)"
+
+if [ "$(id -u)" -ne 0 ]
+then
+  echo 'Elevating to root ...'
+  exec sudo "$0"
+fi
+
+for repo in backports-update backports-debug-update sle-update sle-debug-update update-oss
+do
+  repo="repo-$repo"
+  if echo "$repositories" | grep "$repo"
+  then
+    zypper mr -d "$repo"
+  fi
+done
+
+_zypper ref
+
+# would be nice being able to set this transiently only for the dup
+sed -Ei 's/(DISABLE_RESTART_ON_UPDATE=)"no"/\1"yes"/' /etc/sysconfig/services
+
+_zypper dup --allow-vendor-change  # ugh
+echo "$?"
+
+echo '
+
+  Now do:
+    1. reboot
+    2. systemctl --failed
+    3. salt-call state.apply
+
+'

commit 46ed0f2de49b97489f8b3c0e77d6601f70372a14

update-os: correct source file suffix
Fixes: 157957bc3d6e ("Add basic OS upgrade script")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/profile/update-os/init.sls
@@ -14,7 +14,7 @@
   file.absent
 {%- else %}
   file.managed:
-    - source: salt://{{ slspath }}/files/upgrade-os.sh
+    - source: salt://{{ slspath }}/files/upgrade-os.sh.jinja
     - template: jinja
     - context:
         latest_version: '{{ latest }}'