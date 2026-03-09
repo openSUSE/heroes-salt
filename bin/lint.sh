26eb0fed90d0aaac1672d2f29e6296e96adc8d82
Christian Boltz <cboltz@opensuse.org>
2026-03-09 23:33:15 +0100

    lint.sh: catch all ruff exitcodes
    Only checking for 1 will error out if ruff finds an issue in some python
    script, but it won't error out if ruff.conf has a syntax error (which
    results in exitcode 2)

--- bin/lint.sh
+++ bin/lint.sh
@@ -125,6 +125,13 @@ then
   # shellcheck disable=SC2016
   echo 'Tip: Install `ruff` on your workstation, and use `ruff --fix --unsafe-fixes <file1> <file2> ...` to have the tool automatically implement some of its suggestions.'
   EXIT=5
+elif [ "$STATUS_PYTHON" != 0 ] || [ "$STATUS_PYTHON_PROFILE" != 0 ]
+then
+  echo -n '--> Python: '
+  echo_FAILED
+  echo 'Error running ruff, check the error message above.'
+  echo 'A possible cause for this is invalid syntax in etc/ruff.toml.'
+  EXIT=1
 else
   echo -n '--> Python: '
   echo_PASSED
