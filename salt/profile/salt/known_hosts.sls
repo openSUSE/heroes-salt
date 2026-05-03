openSUSE/heroes-salt

commit a4a8d54ba9dfc4fa74e16f42eafddee370a66898

salt/known_hosts: manage git service key
The new git.i.o.o uses dedicated SSH keys, store them in the script and
introduce a directory for such non-machine keys.
Switch repository to the new location there as well and create a SSH key
for pushing there.

Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/profile/salt/known_hosts.sls
@@ -36,9 +36,15 @@ salt_known_hosts-repository:
     - mode: '0755'
 
   git.cloned:
-    - name: https://gitlab.infra.opensuse.org/infra/ssh_known_hosts.git
+    - name: https://git.infra.opensuse.org/infra/ssh_known_hosts.git
     - target: {{ repo_dir }}
     - branch: main
     - user: salt
     - require:
         - file: {{ repo_base_dir }}
+
+salt_ssh_key:
+  cmd.run:
+    - name: ssh-keygen -f /var/lib/salt/.ssh/id_ed25519 -N '' -t ed25519
+    - runas: salt
+    - creates: /var/lib/salt/.ssh/id_ed25519

salt/profile/salt/files/salt-known_hosts.sh.jinja
@@ -12,19 +12,12 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument('--sshfp', help='Print SSHFP records instead of a known_hosts file', action='store_true')
+parser.add_argument('--static', help='Work with static keys instead of querying the Salt mine', action='store_true')
 args = parser.parse_args()
 
 if args.sshfp:
     from base64 import b64decode
     from hashlib import sha256
-
-import salt.client  # noqa E402, slows down ArgumentParser
-
-client = salt.client.LocalClient()
-this_minion = '{{ grains['fqdn'] }}'
-types = ['ed25519.pub', 'ecdsa.pub']
-
-all_keys = client.cmd(this_minion, 'mine.get', ['*'], kwarg={'fun': 'ssh_host_keys'}).get(this_minion, {})
 
 def mksshfp(host, keytype, key):
     if keytype == 'ssh-ed25519':
@@ -35,11 +28,41 @@ def mksshfp(host, keytype, key):
     #                                   v SHA256
     return f'{host}. IN SSHFP {keytype} 2 {fingerprint}'
 
-for minion, keys in all_keys.items():
-    result = [keys.get(x, '').split()[:2] for x in types if x]
-    for line in result:
-        if args.sshfp:
-            print(mksshfp(minion, line[0], line[1]))
-        else:
-            line = ' '.join(line)
-            print(f'{minion} {line}')
+def print_static():
+  # hosts not collectable through the salt mine
+  for host, keys in {
+    'git.infra.opensuse.org': [
+      'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqC55XDx9dVDBE776o30nvj9m9rMXlIV/44lrJYN1aJ',
+      'ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHR5L8skm3nhekxrERN/g5yvMKVPwr5skdI3IBdRrqCYAQacgCmaslbQk2LhhrsHHDOg1DFQtlKY9nY4DHM+rhk=',
+    ],
+  }.items():
+    for key in keys:
+      if args.sshfp:
+        key_type, key_data = key.split()
+        print(mksshfp(host, key_type, key_data))
+      else:
+        print(f'{host} {key}')
+
+def print_mine():
+  import salt.client  # noqa E402, slows down ArgumentParser
+
+  client = salt.client.LocalClient()
+  this_minion = '{{ grains['fqdn'] }}'
+  types = ['ed25519.pub', 'ecdsa.pub']
+
+  all_keys = client.cmd(this_minion, 'mine.get', ['*'], kwarg={'fun': 'ssh_host_keys'}).get(this_minion, {})
+
+  for minion, keys in all_keys.items():
+      result = [keys.get(x, '').split()[:2] for x in types if x]
+      for line in result:
+          if args.sshfp:
+              print(mksshfp(minion, line[0], line[1]))
+          else:
+              line = ' '.join(line)
+              print(f'{minion} {line}')
+
+if __name__ == '__main__':
+  if args.static:
+    print_static()
+  else:
+    print_mine()

salt/profile/salt/files/salt-update-known_hosts-git.sh.jinja
@@ -23,6 +23,16 @@ then
 	mkdir "$site"
 fi
 
+{#- have only one location manage service keys #}
+{%- if site == 'prg2' %}
+if [ ! -d services ]
+then
+	mkdir services
+fi
+
+{{ base_script }} --static > services/known_hosts
+{%- endif %}
+
 {{ base_script }} > "$site"/known_hosts
 
 if [ -z "$(git status --porcelain)" ]

commit c49b86749f8e39d49257bc477c6c6396e4837592

salt/known_hosts: correct git url
Push happens via SSH now, hence the repository URL must be set
respectively.

Fixes: a4a8d54ba9df ("salt/known_hosts: manage git service key")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

salt/profile/salt/known_hosts.sls
@@ -36,7 +36,7 @@ salt_known_hosts-repository:
     - mode: '0755'
 
   git.cloned:
-    - name: https://git.infra.opensuse.org/infra/ssh_known_hosts.git
+    - name: ssh://git@git.infra.opensuse.org/infra/ssh_known_hosts.git
     - target: {{ repo_dir }}
     - branch: main
     - user: salt