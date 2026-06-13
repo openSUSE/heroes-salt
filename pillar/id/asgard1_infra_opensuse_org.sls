openSUSE/heroes-salt

commit bfe25146bb49850d54447a5ea7220d045eed9bd5

Add asgard management network
This adds an out of band route to asgard, allowing for maintenance
without a dependency on the interface providing network services.

Link: https://progress.opensuse.org/issues/138623
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/id/asgard1_infra_opensuse_org.sls
@@ -48,6 +48,7 @@ sshd_config:
     - fd4b:5292:d67e:1::1
     - fd4b:5292:d67e:3::1
     - fd4b:5292:d67e:1000::1
+    - fd4b:5292:d67e:1003::1
 profile:
   buddycheck:
     buddy: 2a07:de40:b27e:1203::2

pillar/id/asgard2_infra_opensuse_org.sls
@@ -48,6 +48,7 @@ sshd_config:
     - fd4b:5292:d67e:2::1
     - fd4b:5292:d67e:4::1
     - fd4b:5292:d67e:1000::2
+    - fd4b:5292:d67e:1003::2
 profile:
   buddycheck:
     buddy: 2a07:de40:b27e:1203::1

pillar/infra/hosts.yaml
@@ -21,6 +21,10 @@ asgard1:
       mac: d2:e1:4b:98:46:1f
       mode: passthrough
       source: bond-fib
+    os-asgard-m:
+      ip6: fd4b:5292:d67e:1003::1/64
+      mac: d2:e1:4b:98:46:1e
+      source: x-os-asgard-m
   node: orbit20.infra.opensuse.org
   ram: 4096MB
   vcpu: 4
@@ -34,6 +38,10 @@ asgard2:
       mac: d2:e1:4b:98:46:2f
       mode: passthrough
       source: bond-fib
+    os-asgard-m:
+      ip6: fd4b:5292:d67e:1003::2/64
+      mac: d2:e1:4b:98:46:2e
+      source: x-os-asgard-m
   node: orbit21.infra.opensuse.org
   ram: 4096MB
   vcpu: 4
@@ -178,6 +186,10 @@ devcon:
   disks:
     root: 15G
   interfaces:
+    os-asgard-m:
+      ip6: fd4b:5292:d67e:1003::de/64
+      mac: 1a:eb:8a:3d:1a:83
+      source: x-os-asgard-m
     os-internal:
       ip6: 2a07:de40:b27e:1203::de/64
       mac: 1a:eb:8a:3d:1a:80
@@ -1260,6 +1272,10 @@ warp:
   disks:
     root: 15G
   interfaces:
+    os-asgard-m:
+      ip6: fd4b:5292:d67e:1003::a/64
+      mac: d6:f7:c1:31:ce:82
+      source: x-os-asgard-m
     os-s-warp:
       ip6: 2a07:de40:b27e:1101::a/64
       mac: d6:f7:c1:31:ce:80

pillar/cluster/orbit/init.sls
@@ -29,6 +29,7 @@ network:
       firewall: false
 
     # VLAN interfaces for generic (non-VRRP) VM connectivity
+    {{ vlantap('os-asgard-m', 1003, 'bond-ob') }}
     {{ vlantap('os-internal', 1203, 'bond-ob') }}
 
   {{ default_gateway('prg2', 'openSUSE-bare') }}

pillar/cluster/squanchy/init.sls
@@ -24,6 +24,7 @@ network:
             'os-ipmi-ur': 1001,
           },
           'ob': {
+            'os-asgard-m': 1003,
             's-j-os-out': 3202,
             's-na-mgmt': 3339,
           },

salt/files/nftables/asgard/base_inet/input.nft
@@ -17,6 +17,7 @@ vim:filetype=nftables.jinja
 
   iif vmap {
     os-asgard                      : jump network_os-asgard,
+    os-asgard-m                    : jump network_os-asgard-m,
     os-p2p-pub                     : jump input_network_os-p2p-pub,
     {%- for network, network_config in site_networks['prg2'].items() | sort(attribute='1.short') %}
       {%- set shortnet = network_config['short'] %}

salt/files/nftables/asgard/base_inet/output.nft
@@ -11,16 +11,17 @@
   ip protocol icmp accept
 
   oif vmap {
-    os-asgard  : jump network_os-asgard,
-    os-s-warp  : jump output_network_os-s-warp,
-    os-bare    : jump output_network_os-bare,
-    os-p2p-pub : jump output_network_os-p2p-pub,
-    os-mirror  : jump output_network_os-mirror,
-    os-public  : jump output_network_os-public,
-    os-salt    : jump output_network_os-salt,
-    os-thor    : jump output_network_os-thor,
-    os-internal: jump output_network_os-internal,
-    os-log     : jump output_network_os-log,
+    os-asgard   : jump network_os-asgard,
+    os-asgard-m : jump network_os-asgard-m,
+    os-s-warp   : jump output_network_os-s-warp,
+    os-bare     : jump output_network_os-bare,
+    os-p2p-pub  : jump output_network_os-p2p-pub,
+    os-mirror   : jump output_network_os-mirror,
+    os-public   : jump output_network_os-public,
+    os-salt     : jump output_network_os-salt,
+    os-thor     : jump output_network_os-thor,
+    os-internal : jump output_network_os-internal,
+    os-log      : jump output_network_os-log,
   }
 
   oifname nueqsc_stonehat jump output_network_nueqsc

salt/files/nftables/asgard/zones/1003_os-asgard-m.nft
@@ -1,0 +1,10 @@
+###################################################
+## MANAGED BY SALT in salt/files/nftables/asgard ##
+###################################################
+
+chain network_os-asgard-m {
+  ip6 nexthdr icmpv6 accept
+  tcp dport ssh accept
+
+  log prefix "[Asgard Mgmt Denied] " reject with icmpv6 type admin-prohibited
+}

commit 4ae689509b519bebf344e0f6bb1b6ba619e7a5f7

Set Asgard management addresses directly in pillar
The generated network pillar only covers addressing of secondary
interfaces when the primary interface is equipped with addresses too.

Fixes: bfe25146bb49 ("Add asgard management network")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/id/asgard1_infra_opensuse_org.sls
@@ -30,6 +30,9 @@ network:
       addresses:
         - fd4b:5292:d67e:1000::1/64
         - 172.16.128.1/30
+    os-asgard-m:
+      addresses:
+        - fd4b:5292:d67e:1003::1/64
     os-p2p-pub:
       addresses:
         - 195.135.223.44/29

pillar/id/asgard2_infra_opensuse_org.sls
@@ -30,6 +30,9 @@ network:
       addresses:
         - fd4b:5292:d67e:1000::2/64
         - 172.16.128.2/30
+    os-asgard-m:
+      addresses:
+        - fd4b:5292:d67e:1003::2/64
     os-p2p-pub:
       addresses:
         - 195.135.223.45/29

pillar/infra/hosts.yaml
@@ -22,7 +22,6 @@ asgard1:
       mode: passthrough
       source: bond-fib
     os-asgard-m:
-      ip6: fd4b:5292:d67e:1003::1/64
       mac: d2:e1:4b:98:46:1e
       source: x-os-asgard-m
   node: orbit20.infra.opensuse.org
@@ -39,7 +38,6 @@ asgard2:
       mode: passthrough
       source: bond-fib
     os-asgard-m:
-      ip6: fd4b:5292:d67e:1003::2/64
       mac: d2:e1:4b:98:46:2e
       source: x-os-asgard-m
   node: orbit21.infra.opensuse.org