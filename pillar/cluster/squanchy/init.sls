openSUSE/heroes-salt

commit 6f38365177ddb75993683125ac0532c25ad73211

Squanchy: restore unrouted networks
vlantapnetworks looks up data from networks.yaml, which by design only
contains routed networks - the two unrouted entries were hence acting
as no-ops. restore functionality and factor all unrouted networks into
a map for easy future additions.

Fixes: 6cf4862ef9d8 ("Modernize Falkor/Squanchy network pillars")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/squanchy/init.sls
@@ -19,7 +19,20 @@ network:
       firewall: false
 
     # VLAN interface for devcon VM connectivity
-    {{ vlantap('os-ipmi-ur', 1001, 'bond-mgmt') }}
+    {%- set vlanmap_ur = {
+          'mgmt': {
+            'os-ipmi-ur': 1001,
+          },
+          'ob': {
+            's-j-os-out': 3202,
+            's-na-mgmt': 3339,
+          },
+    } %}
+    {%- for bond, vlans in vlanmap_ur.items() %}
+      {%- for vlan_name, vlan_id in vlans.items() %}
+    {{ vlantap(vlan_name, vlan_id, 'bond-' ~ bond) }}
+      {%- enfor %}
+    {%- endfor %}
 
     # VLAN interfaces for generic VM connectivity
     {%- set vlanlist_r = [
@@ -28,8 +41,6 @@ network:
           'os-s-warp',
           'os-salt',
           'os-thor',
-          's-j-os-out',
-          's-na-mgmt',
         ]
     %}
     {{ vlantapnetworks(vlanlist_r, 'bond-ob', 'prg2') }}
@@ -40,7 +51,7 @@ firewalld:
   zones:
     drop:
       interfaces:
-        {%- for vlan_name in vlanlist_r %}
+        {%- for vlan_name in vlanlist_r + vlanmap_ur['mgmt'] | list + vlanmap_ur['ob'] | list %}
         - x-{{ vlan_name }}
         {%- endfor %}
 

commit 35952b7c824e70296a4f0dda034e7c95efcfe52f

Correct Squanchy network loop
Fixes: 6f38365177dd ("Squanchy: restore unrouted networks")
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

pillar/cluster/squanchy/init.sls
@@ -32,7 +32,7 @@ network:
     {%- for bond, vlans in vlanmap_ur.items() %}
       {%- for vlan_name, vlan_id in vlans.items() %}
     {{ vlantap(vlan_name, vlan_id, 'bond-' ~ bond) }}
-      {%- enfor %}
+      {%- endfor %}
     {%- endfor %}
 
     # VLAN interfaces for generic VM connectivity